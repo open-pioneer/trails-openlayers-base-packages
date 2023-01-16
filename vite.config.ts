0ea021ede00119a2cf2fdb35b52f5be6aec8c3d4
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-01-16 11:25:58 +0100

    Add example for vitest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
@@ -11,4 +12,7 @@ export default defineConfig({
         emptyOutDir: true,
     },
     plugins: [react(), eslint()],
+    
+    // https://vitest.dev/config/
+    test: {}
 });
