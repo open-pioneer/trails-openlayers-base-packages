0ffaa02edbc2c161d4a9ad128ea47a29a33b5bdf
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-01-30 10:11:50 +0100

    Set and document browser target versoins

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,17 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 
+// Minimum browser versions supported by generated JS/CSS
+// See also:
+// - https://vitejs.dev/config/build-options.html#build-target
+// - https://esbuild.github.io/api/#target
+const targets = [
+    "chrome92",
+    "edge92",
+    "firefox91",
+    "safari14"
+];
+
 // https://vitejs.dev/config/
 export default defineConfig({
     root: resolve(__dirname, "src"),
@@ -15,6 +26,7 @@ export default defineConfig({
     build: {
         outDir: resolve(__dirname, "dist"),
         emptyOutDir: true,
+        target: targets
     },
 
     plugins: [
