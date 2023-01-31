e68415d5233fef15fcecbc2bb55bd634c9570de4
Antonia van Eek <a.vaneek@conterra.de>
2023-01-31 15:45:03 +0100

    tests for BuildConfigSchema

--- vite-plugin/src/all.test.ts
+++ vite-plugin/src/all.test.ts
@@ -1,4 +1,5 @@
 import "./codegen/generatePackagesMetadata.test";
 import "./codegen/IdGenerator.test";
 import "./codegenPlugin.test";
+import "./metadata/BuildConfigSchema.test";
 import "./mpaPlugin.test";
