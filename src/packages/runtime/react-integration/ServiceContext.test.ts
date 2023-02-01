c47bebff385dcb9ccdd8513d7b26054f3030f512
Jan Schulte <j.schulte@52north.org>
2023-02-01 11:52:27 +0100

    add additional test for undefined service

--- src/packages/runtime/react-integration/ServiceContext.test.ts
+++ src/packages/runtime/react-integration/ServiceContext.test.ts
@@ -1,3 +1,5 @@
+import { ReactIntegration } from "./ReactIntegration";
+import { ServiceLayer } from "./../services/ServiceLayer";
 /**
  * @vitest-environment jsdom
  */
@@ -47,4 +49,34 @@ it("should render component and using service which manipulates dom", async () =
     const customElement = await TestUtils.render("test-elem");
     const selectedElem = await TestUtils.waitForSelector("#wrapper", customElement.shadowRoot!);
     expect(selectedElem.querySelector("span")!.innerHTML).toBe("Hello TEST");
+});
+
+it("should get error while use undefined service", async () => {
+    // TODO: better test for React UI integration
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let resolveCallback: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const errorMessage = new Promise<any>((resolve) => {
+        resolveCallback = resolve;
+    });
+
+    function TestComponent() {
+        try {
+            const service = useServiceInternal("test", "test.Provider") as TestProvider;
+            return createElement("span", undefined, `Hello ${service.value}`);
+        } catch (error) {
+            resolveCallback(error);
+        }
+        return null;
+    }
+    const wrapper = document.createElement("div");
+    const serviceLayer = new ServiceLayer([]);
+    const reactIntegration = new ReactIntegration({
+        rootNode: wrapper,
+        serviceLayer
+    });
+    serviceLayer.start();
+    reactIntegration.render(TestComponent, {});
+    const error = await errorMessage;
+    expect(error.message).toMatchSnapshot();
 });
