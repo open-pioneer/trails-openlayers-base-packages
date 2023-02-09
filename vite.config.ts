a19b41ee11419a642e2fe5e38b08844daf0330c5
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-02-09 13:10:30 +0100

    Add example for 1-to-n dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,19 @@
 import { pioneer } from "@open-pioneer/vite-plugin-pioneer";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import eslint from "vite-plugin-eslint";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // Minimum browser versions supported by generated JS/CSS
 // See also:
 // - https://vitejs.dev/config/build-options.html#build-target
 // - https://esbuild.github.io/api/#target
-const targets = [
-    "chrome92",
-    "edge92",
-    "firefox91",
-    "safari14"
-];
+const targets = ["chrome92", "edge92", "firefox91", "safari14"];
+
+// Generates a stats.html in the output dir to inspect bundle sizes.
+// See also: https://github.com/btd/rollup-plugin-visualizer
+const visualize = false;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -34,16 +34,16 @@ export default defineConfig({
             rootSite: true,
             apps: ["date-app"],
             sites: [
-                "date-sample", 
-                "logging-sample", 
-                "services-sample", 
+                "date-sample",
+                "services-sample",
                 "map-sample",
                 "styling-sample",
                 "chakra-sample"
             ]
         }),
         react(),
-        eslint()
+        eslint(),
+        visualize && (visualizer({ gzipSize: true, brotliSize: true, emitFile: true }) as PluginOption)
     ],
 
     // https://vitest.dev/config/
