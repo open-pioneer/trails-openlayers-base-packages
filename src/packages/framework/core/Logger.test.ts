64846ccdddfadab617d89d2228a84765d5ecb538
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-02-27 10:21:54 +0100

    Don't log in tests if not necessary

--- src/packages/framework/core/Logger.test.ts
+++ src/packages/framework/core/Logger.test.ts
@@ -9,35 +9,35 @@ afterEach(() => {
 
 it("logs complete DEBUG message with additional value", function () {
     const logger = new LoggerImpl("test-prefix:LoggerTest", "DEBUG");
-    const logSpy = vi.spyOn(console, "debug");
+    const logSpy = vi.spyOn(console, "debug").mockImplementation(doNothing);
     logger.debug("Test message", [{ test: 12345 }]);
     expect(logSpy).toHaveBeenCalledWith("test-prefix:LoggerTest: Test message", [{ test: 12345 }]);
 });
 
 it("logs INFO message", function () {
     const logger = new LoggerImpl("test-prefix:LoggerTest", "INFO");
-    const logSpy = vi.spyOn(console, "info");
+    const logSpy = vi.spyOn(console, "info").mockImplementation(doNothing);
     logger.info("Test message");
     expect(logSpy).toHaveBeenCalledWith("test-prefix:LoggerTest: Test message");
 });
 
 it("logs WARN message", function () {
     const logger = new LoggerImpl("test-prefix:LoggerTest", "WARN");
-    const logSpy = vi.spyOn(console, "warn");
+    const logSpy = vi.spyOn(console, "warn").mockImplementation(doNothing);
     logger.warn("Test message");
     expect(logSpy).toHaveBeenCalledWith("test-prefix:LoggerTest: Test message");
 });
 
 it("logs ERROR message", function () {
     const logger = new LoggerImpl("test-prefix:LoggerTest", "WARN");
-    const logSpy = vi.spyOn(console, "error");
+    const logSpy = vi.spyOn(console, "error").mockImplementation(doNothing);
     logger.error("Test message");
     expect(logSpy).toHaveBeenCalledWith("test-prefix:LoggerTest: Test message");
 });
 
 it("logs ERROR message with Error as message", function () {
     const logger = new LoggerImpl("test-prefix:LoggerTest", "ERROR");
-    const logSpy = vi.spyOn(console, "error");
+    const logSpy = vi.spyOn(console, "error").mockImplementation(doNothing);
     const err = new Error("test");
     logger.error(err);
     expect(logSpy).toHaveBeenCalledWith("test-prefix:LoggerTest:", err);
@@ -45,7 +45,7 @@ it("logs ERROR message with Error as message", function () {
 
 it("does not log DEBUG message if WARN level configured", function () {
     const logger = new LoggerImpl("test-prefix:LoggerTest", "WARN");
-    const logSpy = vi.spyOn(console, "debug");
+    const logSpy = vi.spyOn(console, "debug").mockImplementation(doNothing);
     logger.debug("Test message");
     expect(logSpy).toBeCalledTimes(0);
 });
@@ -57,3 +57,7 @@ it("isDebug method depends on log level", function () {
     const errorLogger = new LoggerImpl("test-prefix:LoggerTest", "ERROR");
     expect(errorLogger.isDebug()).toBe(false);
 });
+
+function doNothing() {
+    return;
+}
