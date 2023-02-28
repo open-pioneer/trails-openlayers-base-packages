0a634ee1ef40623eafb15c487c614933aeb1973d
Jan Schulte <j.schulte@52north.org>
2023-02-28 16:29:30 +0100

    move components

--- src/packages/framework/open-layers/index.ts
+++ src/packages/framework/open-layers/index.ts
@@ -1,20 +1,5 @@
 // SPDX-FileCopyrightText: con terra GmbH and contributors
 // SPDX-License-Identifier: Apache-2.0
-// export * from "./api";
-// export { type PackageIntl } from "./i18n";
-// export {
-//     type ApplicationElement,
-//     type ApplicationElementConstructor,
-//     type ApplicationProperties,
-//     type ApplicationConfig,
-//     type CustomElementOptions,
-//     type ConfigContext,
-//     type RawApplicationProperties,
-//     createCustomElement
-// } from "./CustomElement";
-// export * from "./Service";
-// export * from "./ServiceRegistry";
-// export * from "./PropertiesRegistry";
 
-export * from "./MapComponent";
-export * from "./CoordinateComponent";
+export * from "./components/MapComponent";
+export * from "./components/CoordinateComponent";
