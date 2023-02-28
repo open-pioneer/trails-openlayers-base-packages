036a20fe3fc78a361cd8f45a416d04bea7b7744b
Jan Schulte <j.schulte@52north.org>
2023-02-28 16:42:48 +0100

    use chakra to style map app

--- src/samples/map-sample/ol-app/MapApp.tsx
+++ src/samples/map-sample/ol-app/MapApp.tsx
@@ -1,18 +1,33 @@
 // SPDX-FileCopyrightText: con terra GmbH and contributors
 // SPDX-License-Identifier: Apache-2.0
-import { useService } from "open-pioneer:react-hooks";
-import { type MapConfigProvider } from "./services";
+import { Box, Button, Flex, Spacer, useDisclosure } from "@open-pioneer/chakra-integration";
 import { CoordinateComponent, MapComponent } from "@open-pioneer/open-layers";
+import { useService } from "open-pioneer:react-hooks";
+
+import { MapConfigProvider } from "./services";
 
 export function MapApp() {
     const mapConfig = useService("config.MapConfig") as MapConfigProvider;
 
+    const { isOpen, onToggle } = useDisclosure();
+
     return (
-        <div className="map-wrapper">
-            <MapComponent id="test" mapOptions={mapConfig.mapOptions}></MapComponent>
-            <div className="right-bottom">
-                <CoordinateComponent></CoordinateComponent>
-            </div>
-        </div>
+        <Flex height="100%">
+            <Box
+                display="flex"
+                flexDirection="column"
+                padding="10px"
+                width={isOpen ? "auto" : "300px"}
+            >
+                <Spacer></Spacer>
+                <Button onClick={onToggle}>Toggle</Button>
+            </Box>
+            <Box flex="1">
+                <MapComponent id="test" mapOptions={mapConfig.mapOptions}></MapComponent>
+                <div className="right-bottom">
+                    <CoordinateComponent></CoordinateComponent>
+                </div>
+            </Box>
+        </Flex>
     );
 }
