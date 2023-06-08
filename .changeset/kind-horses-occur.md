665027f3b628a24cf12ed3fec5214f81078e2d6a
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-06-08 09:41:13 +0200

    Add a test temporary test package

---
+++ .changeset/kind-horses-occur.md
@@ -0,0 +1,6 @@
+---
+"@open-pioneer/experimental-ol-map": patch
+"@open-pioneer/--test-package": patch
+---
+
+Add an inner test package to test transitive dependencies
---
+++ src/experimental-packages/test-package/services.ts
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: con terra GmbH and contributors
+// SPDX-License-Identifier: Apache-2.0
+export class SayHello {
+    constructor() {
+        console.log("Hello from SayHello service!");
+    }
+}
