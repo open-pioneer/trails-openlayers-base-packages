2a64434f7648426bf7e29c5afd1a05250d1210a0
Bierett, Danilo <danilo.bierett@it.nrw.de>
2023-08-14 13:01:07 +0200

    fix typedoc config

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -6,7 +6,7 @@ const documentedPackages = [
     "experimental-packages/ol-layer-control",
     "experimental-packages/ol-map",
     "packages/authentication",
-    "scale-viewer"
+    "packages/scale-viewer"
 ];
 
 // See https://typedoc.org/options/
