b5a8ad0fc472ab43c22f53ca35c92fd7a3274879
Bierett, Danilo <danilo.bierett@it.nrw.de>
2023-08-15 13:12:46 +0200

    Remove hook export from package

--- src/packages/coordinate-viewer/index.ts
+++ src/packages/coordinate-viewer/index.ts
@@ -1,3 +1,3 @@
 // SPDX-FileCopyrightText: con terra GmbH and contributors
 // SPDX-License-Identifier: Apache-2.0
-export * from "./CoordinateViewer";
+export { CoordinateViewer } from "./CoordinateViewer";
