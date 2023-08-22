221b894066d5bee3992dbcb9d2f7a5d9fb3cc18f
Panayot Nenov <Panayot.Nenov@it.nrw.de>
2023-08-22 18:02:34 +0200

    test for map-api added

---
+++ src/packages/ol-map/MapContainer.test.tsx
@@ -0,0 +1,141 @@
+// SPDX-FileCopyrightText: con terra GmbH and contributors
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * @vitest-environment jsdom
+ */
+import { ServiceOptions } from "@open-pioneer/runtime";
+import {
+    PackageContextProvider,
+    PackageContextProviderProps
+} from "@open-pioneer/test-utils/react";
+import { createService } from "@open-pioneer/test-utils/services";
+import { render, screen, waitFor } from "@testing-library/react";
+import TileLayer from "ol/layer/Tile";
+
+import Stamen from "ol/source/Stamen";
+import { expect, it } from "vitest";
+
+import { MapConfig, MapConfigProvider } from "./api";
+import { MapContainer } from "./MapContainer";
+import { MapRegistryImpl } from "./services";
+
+// used to avoid a "ResizeObserver is not defined" error
+global.ResizeObserver = require("resize-observer-polyfill");
+
+class MapProvider implements MapConfigProvider {
+    mapId = "default";
+    mapConfig: MapConfig = {};
+
+    constructor(options: ServiceOptions) {
+        if (options.properties.mapConfig) {
+            this.mapConfig = options.properties.mapConfig as MapConfig;
+        }
+        if (options.properties.mapId) {
+            this.mapId = options.properties.mapId as string;
+        }
+    }
+
+    getMapConfig(): Promise<MapConfig> {
+        return Promise.resolve(this.mapConfig);
+    }
+}
+
+it("should successfully create a map", async () => {
+    const mapConfigProvider = await createService(MapProvider, {
+        properties: {
+            mapConfig: {} as MapConfig,
+            mapId: "test"
+        }
+    });
+
+    const mapRegistryService = await createService(MapRegistryImpl, {
+        references: {
+            providers: [mapConfigProvider]
+        }
+    });
+
+    const mocks: PackageContextProviderProps = {
+        services: {
+            "ol-map.MapRegistry": mapRegistryService
+        }
+    };
+
+    const renderResult = render(
+        <PackageContextProvider {...mocks}>
+            <div data-testid="base">
+                <MapContainer mapId="test" />
+            </div>
+        </PackageContextProvider>
+    );
+
+    // Assert map is mounted
+    await waitFor(async () => {
+        const div = await screen.findByTestId("base");
+        const container = div.querySelector(".ol-viewport");
+        if (!container) {
+            throw new Error("Map not mounted");
+        }
+    });
+
+    // Div is registered as map target
+    const map = await mapRegistryService.getOlMap("test");
+    const container = renderResult.container.querySelector(".map-container");
+    expect(container).toBeInstanceOf(HTMLDivElement);
+    expect(map?.getTarget()).toBe(container);
+
+    // Unmounting removes the container from the map
+    renderResult.unmount();
+    expect(map?.getTarget()).toBeUndefined();
+});
+//TODO TEST -> should throw an error if the map container is registered twice
+it("should successfully create a map with given configuration", async () => {
+    const mapConfigProvider = await createService(MapProvider, {
+        properties: {
+            mapConfig: {
+                initialView: {
+                    kind: "position",
+                    center: {
+                        x: 123,
+                        y: 456
+                    },
+                    zoom: 5
+                },
+                layers: [
+                    {
+                        title: "testLayer1",
+                        layer: new TileLayer({
+                            source: new Stamen({ layer: "watercolor" }),
+                            properties: { title: "Watercolor" },
+                            visible: false
+                        })
+                    },
+                    {
+                        title: "testLayer2",
+                        layer: new TileLayer({
+                            source: new Stamen({ layer: "toner" }),
+                            properties: { title: "Toner" },
+                            visible: false
+                        })
+                    }
+                ],
+                projection: "EPSG:4326"
+            } as MapConfig,
+            mapId: "test"
+        }
+    });
+
+    const mapRegistryService = await createService(MapRegistryImpl, {
+        references: {
+            providers: [mapConfigProvider]
+        }
+    });
+
+    const map = await mapRegistryService.getOlMap("test");
+    expect(map?.getView().getProjection().getCode()).toBe("EPSG:4326");
+    const layers = map?.getAllLayers();
+    const layer1 = layers?.at(0);
+    const layer2 = layers?.at(1);
+    expect(layers?.length).toBe(2);
+    expect(layer1?.getProperties().title).toBe("Watercolor");
+    expect(layer2?.getProperties().title).toBe("Toner");
+});
---
+++ src/packages/ol-map/MapRegistryImpl.test.ts
@@ -0,0 +1,272 @@
+// SPDX-FileCopyrightText: con terra GmbH and contributors
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * @vitest-environment jsdom
+ */
+
+import { ServiceOptions } from "@open-pioneer/runtime";
+import { registerProjections } from "./projections";
+import { createService } from "@open-pioneer/test-utils/services";
+
+import { afterEach, expect, it, vi } from "vitest";
+
+import { MapConfig, MapConfigProvider } from "./api";
+
+import { MapRegistryImpl } from "./services";
+import { Attribution } from "ol/control";
+import { View } from "ol";
+
+// used to avoid a "ResizeObserver is not defined" error
+global.ResizeObserver = require("resize-observer-polyfill");
+const MAP_ID = "test";
+class MapProvider implements MapConfigProvider {
+    mapId = "default";
+    mapConfig: MapConfig = {};
+
+    constructor(options: ServiceOptions) {
+        if (options.properties.mapConfig) {
+            this.mapConfig = options.properties.mapConfig as MapConfig;
+        }
+        if (options.properties.mapId) {
+            this.mapId = options.properties.mapId as string;
+        }
+    }
+
+    getMapConfig(): Promise<MapConfig> {
+        return Promise.resolve(this.mapConfig);
+    }
+}
+
+afterEach(() => {
+    vi.restoreAllMocks();
+});
+it("should successfully create and destroy a mapModel", async () => {
+    const mapRegistryService = await createMapRegistryService({
+        mapConfig: {} as MapConfig,
+        mapId: MAP_ID
+    });
+    const mapModel = await mapRegistryService.getMapModel(MAP_ID);
+    expect(mapModel?.id).toBe(MAP_ID);
+
+    mapRegistryService.destroy();
+
+    await expect(() =>
+        mapRegistryService.getMapModel(MAP_ID)
+    ).rejects.toThrowErrorMatchingInlineSnapshot('"MapRegistry has already been destroyed."');
+});
+
+it("should successfully set only Attribution when Controls are epmty", async () => {
+    const mapRegistryService = await createMapRegistryService({
+        mapConfig: {} as MapConfig,
+        mapId: MAP_ID
+    });
+    const map = await mapRegistryService.getOlMap(MAP_ID);
+
+    const controls = map?.getControls().getArray();
+    const attributes = map?.getControls().getArray().at(0);
+    expect(controls).toHaveLength(1);
+    expect(attributes).toBeInstanceOf(Attribution);
+});
+
+it("should log warnning message if new View is in advanced configuration and projection is settet", async () => {
+    const logSpy = vi.spyOn(global.console, "warn");
+
+    const view = new View({ center: [405948.17, 5757572.85], zoom: 5 });
+
+    const mapRegistryService = await createMapRegistryService({
+        mapConfig: {
+            advanced: {
+                view
+            },
+            projection: "EPSG:25832"
+        } as MapConfig,
+        mapId: MAP_ID
+    });
+
+    await mapRegistryService.getMapModel(MAP_ID);
+    expect(logSpy).toMatchInlineSnapshot(`
+      [MockFunction warn] {
+        "calls": [
+          [
+            "[WARN] ol-map:createMapModel: The advanced configuration for map id 'test' has provided a fully constructed view instance: projection cannot be applied.
+      Use ViewOptions instead of a View instance.",
+          ],
+        ],
+        "results": [
+          {
+            "type": "return",
+            "value": undefined,
+          },
+        ],
+      }
+    `);
+});
+
+it("should log warnning message if new View is in advanced configuration and initialView is settet", async () => {
+    const logSpy = vi.spyOn(global.console, "warn");
+
+    const view = new View({ center: [405948.17, 5757572.85], zoom: 5 });
+
+    const mapRegistryService = await createMapRegistryService({
+        mapConfig: {
+            advanced: {
+                view
+            },
+            initialView: {
+                kind: "position",
+                center: {
+                    x: 123,
+                    y: 456
+                },
+                zoom: 5
+            }
+        } as MapConfig,
+        mapId: MAP_ID
+    });
+
+    await mapRegistryService.getMapModel(MAP_ID);
+    expect(logSpy).toMatchInlineSnapshot(`
+      [MockFunction warn] {
+        "calls": [
+          [
+            "[WARN] ol-map:createMapModel: The advanced configuration for map id 'test' has provided a fully constructed view instance: initialView cannot be applied.
+      Use ViewOptions instead of a View instance.",
+          ],
+        ],
+        "results": [
+          {
+            "type": "return",
+            "value": undefined,
+          },
+        ],
+      }
+    `);
+});
+
+it("should successfully create View with 'position' property", async () => {
+    const mapRegistryService = await createMapRegistryService({
+        mapConfig: {
+            initialView: {
+                kind: "position",
+                center: {
+                    x: 123,
+                    y: 456
+                },
+                zoom: 5
+            }
+        } as MapConfig,
+        mapId: MAP_ID
+    });
+
+    const map = await mapRegistryService.getOlMap(MAP_ID);
+    const view = map?.getView();
+
+    expect(view?.getCenter()).toContain(123);
+    expect(view?.getZoom()).toBe(5);
+});
+
+it("should successfully create View with 'extent' property", async () => {
+    const extent = {
+        xMax: 653028,
+        xMin: 5986277,
+        yMax: 1674447,
+        yMin: 1674447
+    };
+
+    const mapRegistryService = await createMapRegistryService({
+        mapConfig: {
+            initialView: {
+                kind: "extent",
+                zoom: 5,
+                extent
+            }
+        } as MapConfig,
+        mapId: MAP_ID
+    });
+
+    const xMin = extent.xMin + (extent.xMax - extent.xMin) / 2;
+    const map = await mapRegistryService.getOlMap(MAP_ID);
+    const view = map?.getView();
+
+    expect(view?.getCenter()).toContain(xMin);
+    expect(view?.getZoom()).toBe(0);
+});
+
+it("should successfully create View with 'Default' property", async () => {
+    const mapRegistryService = await createMapRegistryService({
+        mapConfig: {} as MapConfig,
+        mapId: MAP_ID
+    });
+
+    const map = await mapRegistryService.getOlMap(MAP_ID);
+    const view = map?.getView();
+    expect(view?.getProjection().getCode()).toBe("EPSG:3857");
+});
+
+it("should throw an exception by wrong EPSG code", async () => {
+    const logSpy = vi.spyOn(global.console, "error");
+    const mapRegistryService = await createMapRegistryService({
+        mapConfig: {
+            projection: "EPSG:0000000000"
+        } as MapConfig,
+        mapId: MAP_ID
+    });
+
+    await mapRegistryService.getMapModel(MAP_ID);
+    expect(logSpy).toMatchInlineSnapshot(`
+      [MockFunction error] {
+        "calls": [
+          [
+            "[ERROR] ol-map:MapRegistry: Failed to construct map 'test'",
+            [Error: Failed to retrieve projection for code 'EPSG:0000000000'.],
+          ],
+        ],
+        "results": [
+          {
+            "type": "return",
+            "value": undefined,
+          },
+        ],
+      }
+    `);
+});
+
+it("should successfully create View with 'EPSG:25832'", async () => {
+    registerProjections({
+        "EPSG:25832":
+            "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
+    });
+
+    const mapRegistryService = await createMapRegistryService({
+        mapConfig: {
+            initialView: {
+                kind: "position",
+                center: {
+                    x: 123,
+                    y: 456
+                },
+                zoom: 5
+            },
+            projection: "EPSG:25832"
+        } as MapConfig,
+
+        mapId: MAP_ID
+    });
+
+    const map = await mapRegistryService.getOlMap(MAP_ID);
+    const view = map?.getView();
+    expect(view?.getProjection().getCode()).toBe("EPSG:25832");
+});
+
+async function createMapRegistryService(properties: Record<string, unknown>) {
+    const mapConfigProvider = await createService(MapProvider, {
+        properties
+    });
+    const mapRegistryService = await createService(MapRegistryImpl, {
+        references: {
+            providers: [mapConfigProvider]
+        }
+    });
+
+    return mapRegistryService;
+}
