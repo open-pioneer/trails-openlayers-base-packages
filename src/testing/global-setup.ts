2643dd0c58fdf769c4718c1d143e9aa8d1bd0503
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-09-12 14:14:46 +0200

    Add comment

--- src/testing/global-setup.ts
+++ src/testing/global-setup.ts
@@ -1,5 +1,10 @@
 // SPDX-FileCopyrightText: con terra GmbH and contributors
 // SPDX-License-Identifier: Apache-2.0
+/**
+ * This file is automatically executed by vitest before the actual test suite is run.
+ * See `vite.config.ts` and https://vitest.dev/config/#setupfiles
+ */
+
 if (typeof window !== "undefined") {
     // Running with mocked dom (happy-dom or jsdom)
     if (!globalThis.ResizeObserver) {
