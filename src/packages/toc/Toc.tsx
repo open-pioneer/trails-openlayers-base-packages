b98de6d62821f2c97dfb96bb16e0bf02dd3dff20
Antonia van Eek <a.vaneek@conterra.de>
2023-09-26 16:39:09 +0200

    started implementing LayerList in toc

--- src/packages/toc/Toc.tsx
+++ src/packages/toc/Toc.tsx
@@ -1,11 +1,21 @@
 // SPDX-FileCopyrightText: con terra GmbH and contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { Box, Text, BoxProps, FormControl, FormLabel } from "@open-pioneer/chakra-integration";
-import { FC, ForwardedRef, forwardRef, RefAttributes } from "react";
+import { Box, Text, BoxProps, Heading } from "@open-pioneer/chakra-integration";
+import {
+    FC,
+    ForwardedRef,
+    forwardRef,
+    RefAttributes,
+    useCallback,
+    useRef,
+    useSyncExternalStore
+} from "react";
 import classNames from "classnames";
 import { BasemapSwitcher, BasemapSwitcherProps } from "@open-pioneer/basemap-switcher";
 import { useIntl } from "open-pioneer:react-hooks";
+import { LayerModel, MapModel, useMapModel } from "@open-pioneer/map";
+
 export interface TocProps extends BoxProps, RefAttributes<HTMLDivElement> {
     /**
      * The id of the map.
@@ -40,6 +50,21 @@ export const Toc: FC<TocProps> = forwardRef(function Toc(
     const basemapsLabel = intl.formatMessage({ id: "basemapsLabel" });
     const tocTitel = intl.formatMessage({ id: "tocTitel" });
 
+    const state = useMapModel(mapId);
+
+    let content;
+    switch (state.kind) {
+        case "loading":
+            content = <div></div>;
+            break;
+        case "rejected":
+            content = <div>{intl.formatMessage({ id: "error" })}</div>;
+            break;
+        case "resolved":
+            content = <LayerList map={state.map!} {...props} />;
+            break;
+    }
+
     return (
         <Box className={classNames("toc", className)} ref={ref} {...rest}>
             {/*TODO: remove header as it should be added by an app developer if needed? */}
@@ -51,15 +76,90 @@ export const Toc: FC<TocProps> = forwardRef(function Toc(
                 <Text as="b">{tocTitel}</Text>
             </Box>
             {hideBasemapSwitcher || (
-                <Box className="toc-content" padding={2}>
-                    <FormControl>
-                        <FormLabel ps={1}>
-                            <Text as="b">{basemapsLabel}:</Text>
-                        </FormLabel>
-                        <BasemapSwitcher {...basemapSwitcherProps} mapId={mapId}></BasemapSwitcher>
-                    </FormControl>
+                /**
+                 *  todo use useId to generate ID
+                 *  todo test is basemapSwitcherProps are applied correctly (even with aria-labelledby)
+                 * todo use TitledSection instead of Heading:
+                 * <TitledSection
+                 *     title={
+                 *         <SectionHeading size="4xl">Heading</SectionHeading>
+                 *     }
+                 * >
+                 *     Content
+                 * </TitledSection>
+                 * todo fix tests when done
+                 */
+                <Box className="toc-basemap-switcher" padding={2}>
+                    <Heading as={"h2"} id={"sljdkf"}>
+                        lsjdfl
+                    </Heading>
+                    <BasemapSwitcher
+                        aria-labelledby={"sljdkf"}
+                        {...basemapSwitcherProps}
+                        mapId={mapId}
+                    ></BasemapSwitcher>
                 </Box>
             )}
+            {/*todo use TitledSection instead of Heading: */}
+            <Box padding={2}>
+                <Heading>Test</Heading>
+                {content}
+            </Box>
         </Box>
     );
+
+    function LayerList(props: { map: MapModel }): JSX.Element {
+        const { map } = props;
+        const layers = useLayers(map);
+
+        // todo use chakra ListItem with check-box inside (use checkbox label for layer titel)
+        // todo ensure to sync with map model
+
+        return (
+            <div className="layer-list">
+                {layers.map((layer, i) => (
+                    <div key={i} className="layer-entry">
+                        {layer.title}
+                    </div>
+                ))}
+            </div>
+        );
+    }
 });
+
+function useLayers(mapModel: MapModel): LayerModel[] {
+    // Caches potentially expensive layers arrays.
+    // Not sure if this is a good idea, but getSnapshot() should always be fast.
+    // If this is a no-go, make getAllLayers() fast instead.
+    const flatOperationalLayers = useRef<LayerModel[] | undefined>();
+    const subscribe = useCallback(
+        (cb: () => void) => {
+            // Reset cache when (re-) subscribing
+            flatOperationalLayers.current = undefined;
+
+            if (!mapModel) {
+                return () => undefined;
+            }
+            const resource = mapModel.layers.on("changed", () => {
+                // Reset cache content so getSnapshot() fetches layers again.
+                flatOperationalLayers.current = undefined;
+                cb();
+            });
+            return () => resource.destroy();
+        },
+        [mapModel]
+    );
+    const getSnapshot = useCallback(() => {
+        if (flatOperationalLayers.current) {
+            return flatOperationalLayers.current;
+        }
+        // todo skip groupLayer, skip basemaps
+
+        const operationalLayers = mapModel?.layers.getOperationalLayers() ?? [];
+        operationalLayers.filter((layerModelImpl) => {});
+        flatOperationalLayers.current = operationalLayers;
+
+        return flatOperationalLayers.current;
+    }, [mapModel]);
+    return useSyncExternalStore(subscribe, getSnapshot);
+}
