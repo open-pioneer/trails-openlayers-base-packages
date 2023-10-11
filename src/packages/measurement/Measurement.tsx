6be7759b9082eb3a4ed881a6bcac9c982025a53b
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-10-11 15:06:21 +0200

    Refactor

--- src/packages/measurement/Measurement.tsx
+++ src/packages/measurement/Measurement.tsx
@@ -9,22 +9,11 @@ import {
     HStack,
     Select
 } from "@open-pioneer/chakra-integration";
-import { FC, useEffect, useState } from "react";
-import { useIntl } from "open-pioneer:react-hooks";
+import { useMapModel } from "@open-pioneer/map";
 import { CommonComponentProps, useCommonComponentProps } from "@open-pioneer/react-utils";
-import { MapModel, useMapModel } from "@open-pioneer/map";
-import Feature from "ol/Feature";
-import Draw from "ol/interaction/Draw";
-import { Vector as VectorSource } from "ol/source";
-import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
-import { LineString, Polygon } from "ol/geom";
-import { getArea, getLength } from "ol/sphere";
-import Overlay from "ol/Overlay";
-import { unByKey } from "ol/Observable";
-import { EventsKey } from "ol/events";
-import { Coordinate } from "ol/coordinate";
-import { Vector as VectorLayer } from "ol/layer";
-import { MapBrowserEvent } from "ol";
+import { useIntl } from "open-pioneer:react-hooks";
+import { FC, useEffect, useState } from "react";
+import { MeasurementController, MeasurementType } from "./MeasurementController";
 
 /**
  * This is for special properties of the Measurement.
@@ -37,49 +26,6 @@ export interface MeasurementProps extends CommonComponentProps {
 }
 
 /**
- * Stores instance of `Draw` interaction. it is global so we can remove it later
- */
-let draw: Draw | null = null;
-
-let layer: VectorLayer<VectorSource> | null = null;
-let source: VectorSource | undefined = undefined;
-
-/**
- * Currently drawn feature.
- */
-let sketch: Feature | null = null;
-
-/**
- * The help tooltip element.
- */
-let helpTooltipElement: HTMLElement;
-
-/**
- * Overlay to show the help messages.
- */
-let helpTooltip: Overlay;
-
-/**
- * The measure tooltip element.
- */
-let measureTooltipElement: HTMLElement | null = null;
-
-/**
- * Overlay to show the measurement.
- */
-let measureTooltip: Overlay;
-
-/**
- * Message to show when the user is drawing a polygon.
- */
-const continuePolygonMsg: string = "Click to continue drawing the polygon";
-
-/**
- * Message to show when the user is drawing a line.
- */
-const continueLineMsg: string = "Click to continue drawing the line";
-
-/**
  * The `Measurement` component can be used in an app to switch between distance and area measurements and to delete all current measurements.
  */
 export const Measurement: FC<MeasurementProps> = (props) => {
@@ -87,29 +33,46 @@ export const Measurement: FC<MeasurementProps> = (props) => {
 
     const { mapId } = props;
     const { containerProps } = useCommonComponentProps("measurement", props);
-    const [selectedMeasurement, setMeasurement] = useState("distance");
+    const [selectedMeasurement, setMeasurement] = useState<MeasurementType>("distance");
     const label = (id: string) => intl.formatMessage({ id: id });
 
     const state = useMapModel(mapId);
     const map = state.map;
+    const [controller, setController] = useState<MeasurementController | undefined>(undefined);
+    useEffect(() => {
+        if (!map) {
+            return;
+        }
+
+        const controller = new MeasurementController(map.olMap, intl);
+        setController(controller);
+        return () => {
+            controller.destroy();
+            setController(undefined);
+        };
+    }, [map, intl]);
 
     useEffect(() => {
-        setUp(map);
-        return () => {
-            cleanUp(map);
-        };
-    }, [map]);
+        if (!controller) {
+            return;
+        }
 
-    useEffect(() => {
-        if (!map) return;
-        addInteraction(selectedMeasurement, map);
+        controller.startMeasurement(selectedMeasurement);
         return () => {
-            draw && map?.olMap.removeInteraction(draw);
+            controller.stopMeasurement();
         };
-    }, [map, selectedMeasurement]);
+    }, [controller, selectedMeasurement]);
 
-    function changeMeasurement(measurement: string) {
-        setMeasurement(measurement);
+    function changeMeasurement(measurementType: string) {
+        if (measurementType === "distance" || measurementType === "area") {
+            setMeasurement(measurementType);
+        } else {
+            throw new Error(`Unexpected measurement type: '${measurementType}'.`);
+        }
+    }
+
+    function removeMeasurements() {
+        controller?.clearMeasurements();
     }
 
     return (
@@ -131,7 +94,7 @@ export const Measurement: FC<MeasurementProps> = (props) => {
                 <Button
                     padding={2}
                     className="delete-measurements"
-                    onClick={() => removeMeasurements(map, selectedMeasurement)}
+                    onClick={removeMeasurements}
                     width="100%"
                 >
                     {label("deleteMeasurementLabel")}
@@ -140,195 +103,3 @@ export const Measurement: FC<MeasurementProps> = (props) => {
         </Box>
     );
 };
-
-function removeMeasurements(map: MapModel | undefined, measurement: string) {
-    draw && map?.olMap.removeInteraction(draw);
-    map?.olMap.getOverlays().clear();
-    layer?.getSource()?.clear();
-    addInteraction(measurement, map);
-}
-
-function setUp(map: MapModel | undefined) {
-    source = new VectorSource();
-    source && createLayer();
-    layer && map?.olMap.addLayer(layer);
-    map?.olMap.on("pointermove", pointerMoveHandler);
-    map?.olMap.getViewport().addEventListener("mouseout", function () {
-        helpTooltipElement.classList.add("hidden");
-    });
-}
-
-function cleanUp(map: MapModel | undefined) {
-    layer && map?.olMap.removeLayer(layer);
-    map?.olMap.getOverlays().clear();
-}
-
-function addInteraction(geometryType: string, map: MapModel | undefined) {
-    const type = geometryType == "area" ? "Polygon" : "LineString";
-
-    let changeListener: EventsKey | undefined = undefined;
-    const style = getStyle();
-
-    draw = new Draw({
-        source: source || undefined,
-        type: type,
-        style: function (feature) {
-            const geometryType = feature?.getGeometry()?.getType();
-            if (geometryType === type || geometryType === "Point") {
-                return style;
-            }
-        }
-    });
-    map?.olMap.addInteraction(draw);
-
-    createMeasureTooltip(map);
-    createHelpTooltip(map);
-
-    draw.on("drawstart", function (evt) {
-        // set sketch
-        sketch = evt.feature;
-
-        let tooltipCoord: Coordinate | null = null;
-
-        changeListener = sketch?.getGeometry()?.on("change", function (evt) {
-            const geom = evt.target;
-            let output;
-            if (geom instanceof Polygon) {
-                output = formatArea(geom);
-                tooltipCoord = geom.getInteriorPoint().getCoordinates() || null;
-            } else if (geom instanceof LineString) {
-                output = formatLength(geom);
-                tooltipCoord = geom.getLastCoordinate() || null;
-            }
-            if (measureTooltipElement) {
-                measureTooltipElement.innerHTML = output || "";
-            }
-            measureTooltip && tooltipCoord && measureTooltip.setPosition(tooltipCoord);
-        });
-    });
-
-    draw.on("drawend", function () {
-        if (measureTooltipElement) {
-            measureTooltipElement.className = "ol-tooltip ol-tooltip-static";
-        }
-        measureTooltip && measureTooltip.setOffset([0, -7]);
-        // unset sketch
-        sketch = null;
-        // unset tooltip so that a new one can be created
-        measureTooltipElement = null;
-        createMeasureTooltip(map);
-        changeListener && unByKey(changeListener);
-    });
-}
-
-const pointerMoveHandler = function (evt: MapBrowserEvent<UIEvent>) {
-    if (evt.dragging) {
-        return;
-    }
-    let helpMsg: string = "Click to start drawing";
-
-    if (sketch) {
-        const geom = sketch.getGeometry();
-        if (geom instanceof Polygon) {
-            helpMsg = continuePolygonMsg;
-        } else if (geom instanceof LineString) {
-            helpMsg = continueLineMsg;
-        }
-    }
-
-    helpTooltipElement.innerHTML = helpMsg;
-    helpTooltip.setPosition(evt.coordinate);
-
-    helpTooltipElement.classList.remove("hidden");
-};
-
-/**
- * Creates a new help tooltip
- */
-function createHelpTooltip(map: MapModel | undefined) {
-    if (helpTooltipElement) {
-        helpTooltipElement.parentNode?.removeChild(helpTooltipElement);
-    }
-    helpTooltipElement = document.createElement("div");
-    helpTooltipElement.className = "ol-tooltip hidden";
-    helpTooltip = new Overlay({
-        element: helpTooltipElement,
-        offset: [15, 0],
-        positioning: "center-left"
-    });
-    map?.olMap.addOverlay(helpTooltip);
-}
-
-/**
- * Creates a new measure tooltip
- */
-function createMeasureTooltip(map: MapModel | undefined) {
-    if (measureTooltipElement) {
-        measureTooltipElement?.parentNode?.removeChild(measureTooltipElement);
-    }
-    measureTooltipElement = document.createElement("div");
-    measureTooltipElement.className = "ol-tooltip ol-tooltip-measure";
-    measureTooltip = new Overlay({
-        element: measureTooltipElement,
-        offset: [0, -15],
-        positioning: "bottom-center",
-        stopEvent: false,
-        insertFirst: false
-    });
-    map?.olMap.addOverlay(measureTooltip);
-}
-function getStyle() {
-    return new Style({
-        fill: new Fill({
-            color: "rgba(255, 255, 255, 0.2)"
-        }),
-        stroke: new Stroke({
-            color: "rgba(0, 0, 0, 0.5)",
-            lineDash: [10, 10],
-            width: 2
-        }),
-        image: new CircleStyle({
-            radius: 5,
-            stroke: new Stroke({
-                color: "rgba(0, 0, 0, 0.7)"
-            }),
-            fill: new Fill({
-                color: "rgba(255, 255, 255, 0.2)"
-            })
-        })
-    });
-}
-
-function formatArea(polygon: Polygon) {
-    const area = getArea(polygon);
-    let output;
-    if (area >= 1000000) {
-        output = Math.round((area / 1000000) * 100) / 100 + " " + "km<sup>2</sup>";
-    } else {
-        output = Math.round(area * 100) / 100 + " " + "m<sup>2</sup>";
-    }
-    return output;
-}
-function formatLength(line: LineString) {
-    const length = getLength(line);
-    let output;
-    if (length >= 1000) {
-        output = Math.round((length / 1000) * 100) / 100 + " " + "km";
-    } else {
-        output = Math.round(length * 100) / 100 + " " + "m";
-    }
-    return output;
-}
-
-function createLayer() {
-    layer = new VectorLayer({
-        source: source,
-        style: {
-            "fill-color": "rgba(255, 255, 255, 0.2)",
-            "stroke-color": "#ffcc33",
-            "stroke-width": 2,
-            "circle-radius": 7,
-            "circle-fill-color": "#ffcc33"
-        }
-    });
-}
---
+++ src/packages/measurement/MeasurementController.ts
@@ -0,0 +1,339 @@
+// SPDX-FileCopyrightText: con terra GmbH and contributors
+// SPDX-License-Identifier: Apache-2.0
+import { Resource } from "@open-pioneer/core";
+import { PackageIntl } from "@open-pioneer/runtime";
+import Feature from "ol/Feature";
+import OlMap from "ol/Map";
+import MapBrowserEvent from "ol/MapBrowserEvent";
+import { unByKey } from "ol/Observable";
+import Overlay from "ol/Overlay";
+import { Coordinate } from "ol/coordinate";
+import { EventsKey } from "ol/events";
+import { LineString, Polygon } from "ol/geom";
+import Draw from "ol/interaction/Draw";
+import { Vector as VectorLayer } from "ol/layer";
+import { Vector as VectorSource } from "ol/source";
+import { getArea, getLength } from "ol/sphere";
+import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
+
+export type MeasurementType = "area" | "distance";
+
+export class MeasurementController {
+    private intl: PackageIntl;
+    private olMap: OlMap;
+
+    /**
+     * The layer rendering the measurement "features".
+     */
+    private layer: VectorLayer<VectorSource>;
+
+    /**
+     * Source of {@link layer}.
+     */
+    private source: VectorSource;
+
+    /**
+     * Stores the current `Draw` interaction.
+     */
+    private draw: Draw | undefined = undefined;
+
+    /**
+     * Currently drawn feature.
+     */
+    private sketch: Feature | undefined = undefined;
+
+    /**
+     * The help tooltip element.
+     */
+    private helpTooltip: Tooltip;
+
+    /**
+     * The current measurement tooltip (if any).
+     */
+    private measureTooltip: Tooltip | undefined;
+
+    /**
+     * Overlays/Tooltip containing the tooltip for finished drawings.
+     */
+    private overlayTooltips: Tooltip[] = [];
+
+    /**
+     * Keeps track of registered event handlers.
+     */
+    private resources: Resource[] = [];
+
+    constructor(olMap: OlMap, intl: PackageIntl) {
+        this.olMap = olMap;
+        this.intl = intl;
+
+        const source = (this.source = new VectorSource());
+        this.layer = new VectorLayer({
+            source,
+            style: {
+                "fill-color": "rgba(255, 255, 255, 0.2)",
+                "stroke-color": "#ffcc33",
+                "stroke-width": 2,
+                "circle-radius": 7,
+                "circle-fill-color": "#ffcc33"
+            }
+        });
+        olMap.addLayer(this.layer);
+
+        const pointerMoveKey = olMap.on("pointermove", this.handlePointerMove.bind(this));
+        this.resources.push({
+            destroy() {
+                unByKey(pointerMoveKey);
+            }
+        });
+
+        const mouseOutHandler = () => {
+            this.helpTooltip.element.classList.add("hidden");
+        };
+        olMap.getViewport().addEventListener("mouseout", mouseOutHandler);
+        this.resources.push({
+            destroy() {
+                olMap.getViewport().removeEventListener("mouseout", mouseOutHandler);
+            }
+        });
+
+        this.helpTooltip = this.createHelpTooltip();
+    }
+
+    destroy() {
+        this.stopMeasurement();
+
+        // Cleanup registered event handlers etc.
+        for (const resource of this.resources) {
+            resource.destroy();
+        }
+        this.resources = [];
+
+        // Cleanup tooltips
+        this.helpTooltip.destroy();
+        this.measureTooltip?.destroy();
+        for (const tooltip of this.overlayTooltips) {
+            tooltip.destroy();
+        }
+        this.overlayTooltips = [];
+
+        // Cleanup layer
+        this.olMap.removeLayer(this.layer);
+        this.layer.dispose();
+        this.source.dispose();
+    }
+
+    clearMeasurements() {
+        // this.stopMeasurement();
+        this.source.clear();
+        for (const tooltip of this.overlayTooltips) {
+            tooltip.destroy();
+        }
+        this.overlayTooltips = [];
+    }
+
+    startMeasurement(type: MeasurementType) {
+        if (this.draw) {
+            throw new Error("Internal error: another measurement interaction is still active.");
+        }
+
+        const geometryType = type === "area" ? "Polygon" : "LineString";
+        const style = getStyle();
+        const draw = (this.draw = new Draw({
+            source: this.source,
+            type: geometryType,
+            style: function (feature) {
+                const featureGeometryType = feature?.getGeometry()?.getType();
+                if (featureGeometryType === geometryType || featureGeometryType === "Point") {
+                    return style;
+                }
+            }
+        }));
+        this.olMap.addInteraction(draw);
+
+        let measureTooltip: Tooltip | undefined;
+
+        let changeListenerKey: EventsKey | undefined = undefined;
+        draw.on("drawstart", (evt) => {
+            measureTooltip = this.measureTooltip = this.createMeasureTooltip();
+
+            // set sketch
+            const sketch = (this.sketch = evt.feature);
+            if (!sketch) {
+                return;
+            }
+
+            let tooltipCoord: Coordinate | undefined;
+            changeListenerKey = sketch.getGeometry()?.on("change", (evt) => {
+                const geom = evt.target;
+                let output = "";
+                if (geom instanceof Polygon) {
+                    output = formatArea(geom);
+                    tooltipCoord = geom.getInteriorPoint().getCoordinates() || null;
+                } else if (geom instanceof LineString) {
+                    output = formatLength(geom);
+                    tooltipCoord = geom.getLastCoordinate() || null;
+                }
+
+                if (measureTooltip) {
+                    measureTooltip.element.innerHTML = output;
+                    if (tooltipCoord) {
+                        measureTooltip?.overlay.setPosition(tooltipCoord);
+                    }
+                }
+            });
+        });
+
+        draw.on("drawend", () => {
+            if (measureTooltip) {
+                // Make the tooltip (which was previously following the mouse)
+                // static instead, next to the feature (last known overlay coordinate).
+                const classes = measureTooltip.element.classList;
+                classes.remove("ol-tooltip-measure");
+                classes.add("ol-tooltip-static");
+                measureTooltip.overlay.setOffset([0, -7]);
+
+                this.overlayTooltips.push(measureTooltip);
+                this.measureTooltip = measureTooltip = undefined;
+            }
+
+            // unset sketch
+            this.sketch = undefined;
+            if (changeListenerKey) {
+                unByKey(changeListenerKey);
+            }
+        });
+    }
+
+    stopMeasurement() {
+        if (this.draw) {
+            this.olMap.removeInteraction(this.draw);
+            this.draw.abortDrawing();
+            this.draw.dispose();
+            this.draw = undefined;
+        }
+
+        this.sketch = undefined;
+        this.measureTooltip?.destroy();
+        this.measureTooltip = undefined;
+    }
+
+    private createMeasureTooltip(): Tooltip {
+        const element = document.createElement("div");
+        element.className = "ol-tooltip ol-tooltip-measure";
+
+        const overlay = new Overlay({
+            element: element,
+            offset: [0, -15],
+            positioning: "bottom-center",
+            stopEvent: false,
+            insertFirst: false
+        });
+
+        const olMap = this.olMap;
+        olMap.addOverlay(overlay);
+        return {
+            overlay,
+            element,
+            destroy() {
+                olMap.removeOverlay(overlay);
+            }
+        };
+    }
+
+    private createHelpTooltip(): Tooltip {
+        const element = document.createElement("div");
+        element.className = "ol-tooltip hidden";
+
+        const overlay = new Overlay({
+            element: element,
+            offset: [15, 0],
+            positioning: "center-left"
+        });
+
+        const olMap = this.olMap;
+        olMap.addOverlay(overlay);
+        return {
+            overlay,
+            element,
+            destroy() {
+                olMap.removeOverlay(overlay);
+            }
+        };
+    }
+
+    private handlePointerMove(evt: MapBrowserEvent<UIEvent>) {
+        if (evt.dragging) {
+            return;
+        }
+
+        const tooltip = this.helpTooltip;
+        const helpMessage = getHelpMessage(this.intl, this.sketch);
+        tooltip.element.textContent = helpMessage;
+        tooltip.overlay.setPosition(evt.coordinate);
+        tooltip.element.classList.remove("hidden");
+    }
+}
+
+/** Represents a tooltip rendered on the OpenLayers map. */
+interface Tooltip extends Resource {
+    overlay: Overlay;
+    element: HTMLDivElement;
+}
+
+function getHelpMessage(intl: PackageIntl, sketch: Feature | undefined) {
+    if (sketch) {
+        const geom = sketch.getGeometry();
+        if (geom instanceof Polygon || geom instanceof LineString) {
+            return intl.formatMessage({
+                id: "tooltips.continue"
+            });
+        }
+    }
+    return intl.formatMessage({
+        id: "tooltips.help"
+    });
+}
+
+function formatArea(polygon: Polygon) {
+    const area = getArea(polygon);
+    let output;
+    if (area >= 1000000) {
+        output = Math.round((area / 1000000) * 100) / 100 + " " + "km<sup>2</sup>";
+    } else {
+        output = Math.round(area * 100) / 100 + " " + "m<sup>2</sup>";
+    }
+    return output;
+}
+
+function formatLength(line: LineString) {
+    const length = getLength(line);
+    let output;
+    if (length >= 1000) {
+        output = Math.round((length / 1000) * 100) / 100 + " " + "km";
+    } else {
+        output = Math.round(length * 100) / 100 + " " + "m";
+    }
+    return output;
+}
+
+function getStyle() {
+    return new Style({
+        fill: new Fill({
+            color: "rgba(255, 255, 255, 0.2)"
+        }),
+        stroke: new Stroke({
+            color: "rgba(0, 0, 0, 0.5)",
+            lineDash: [10, 10],
+            width: 2
+        }),
+        image: new CircleStyle({
+            radius: 5,
+            stroke: new Stroke({
+                color: "rgba(0, 0, 0, 0.7)"
+            }),
+            fill: new Fill({
+                color: "rgba(255, 255, 255, 0.2)"
+            })
+        })
+    });
+}
