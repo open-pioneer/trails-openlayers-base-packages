2d47ee1d74890506046e06bc8a500ffb59b24e07
Senait <s.alema@conterra.de>
2023-11-07 13:23:43 +0100

    no need to test all basemap layer types

--- src/packages/overview-map/OverviewMap.test.tsx
+++ src/packages/overview-map/OverviewMap.test.tsx
@@ -11,12 +11,7 @@ import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import { BkgTopPlusOpen } from "@open-pioneer/map";
 import WMTS from "ol/source/WMTS";
-import { createVectorSource } from "@open-pioneer/ogc-features";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import ImageLayer from "ol/layer/Image";
-import ImageWMS from "ol/source/ImageWMS";
-import { MapboxVectorLayer } from "ol-mapbox-style";
+import OlMap from "ol/Map";
 
 it("should successfully create a overview map component", async () => {
     const { mapId, registry } = await setupMap();
@@ -76,8 +71,7 @@ it("should successfully add OverviewMap control to the map controls", async () =
     );
 
     await waitForOverviewMap();
-    const controls = map.olMap.getControls().getArray();
-    const overViewMapControl = controls.find((control) => control instanceof OlOverviewMap);
+    const overViewMapControl = getControl(map.olMap);
     expect(overViewMapControl).toBeDefined();
 });
 
@@ -99,10 +93,7 @@ it("should support basemap type of OGC WMTS layer as a layer shown in the overvi
     );
 
     await waitForOverviewMap();
-    const controls = map.olMap.getControls().getArray();
-    const overViewMapControl = controls.find((control) => control instanceof OlOverviewMap) as
-        | OlOverviewMap
-        | undefined;
+    const overViewMapControl = getControl(map.olMap);
     const wmtsLayer: TileLayer<WMTS> = overViewMapControl
         ?.getOverviewMap()
         ?.getLayers()
@@ -111,100 +102,6 @@ it("should support basemap type of OGC WMTS layer as a layer shown in the overvi
 
     expect(wmtsLayer).toBeInstanceOf(TileLayer);
     expect(source).toBeInstanceOf(WMTS);
-});
-
-it("should support basemap type of OGC API Feature layer as a layer shown in the overview map", async () => {
-    const { mapId, registry } = await setupMap();
-    const map = await registry.expectMapModel(mapId);
-    const injectedServices = createServiceOptions({ registry });
-    const layer = getOGCAPIFeatureLayer();
-
-    render(
-        <PackageContextProvider services={injectedServices}>
-            <OverviewMap
-                mapId={mapId}
-                layer={layer}
-                className="test"
-                data-testid="overview-map"
-            ></OverviewMap>
-        </PackageContextProvider>
-    );
-
-    await waitForOverviewMap();
-    const controls = map.olMap.getControls().getArray();
-    const overViewMapControl = controls.find((control) => control instanceof OlOverviewMap) as
-        | OlOverviewMap
-        | undefined;
-    const ogcApiFeatureLayer: VectorLayer<VectorSource> = overViewMapControl
-        ?.getOverviewMap()
-        ?.getLayers()
-        .getArray()[0] as VectorLayer<VectorSource>;
-    const source = ogcApiFeatureLayer?.getSource();
-
-    expect(ogcApiFeatureLayer).toBeInstanceOf(VectorLayer);
-    expect(source).toBeInstanceOf(VectorSource);
-});
-
-it("should support basemap type of OGC API Feature layer with MapBox-Styles as a layer shown in the overview map", async () => {
-    const { mapId, registry } = await setupMap();
-    const map = await registry.expectMapModel(mapId);
-    const injectedServices = createServiceOptions({ registry });
-    const layer = getOGCAPIFeatureLayerWithMapboxStyle();
-
-    render(
-        <PackageContextProvider services={injectedServices}>
-            <OverviewMap
-                mapId={mapId}
-                layer={layer}
-                className="test"
-                data-testid="overview-map"
-            ></OverviewMap>
-        </PackageContextProvider>
-    );
-
-    await waitForOverviewMap();
-    const controls = map.olMap.getControls().getArray();
-    const overViewMapControl = controls.find((control) => control instanceof OlOverviewMap) as
-        | OlOverviewMap
-        | undefined;
-    const mapboxVectorLayer: MapboxVectorLayer = overViewMapControl
-        ?.getOverviewMap()
-        ?.getLayers()
-        .getArray()[0] as MapboxVectorLayer;
-
-    expect(mapboxVectorLayer).toBeInstanceOf(MapboxVectorLayer);
-});
-
-it("should support basemap type of OGC WMS layer as a layer shown in the overview map", async () => {
-    const { mapId, registry } = await setupMap();
-    const map = await registry.expectMapModel(mapId);
-    const injectedServices = createServiceOptions({ registry });
-    const layer = getOGCWMSLayer();
-
-    render(
-        <PackageContextProvider services={injectedServices}>
-            <OverviewMap
-                mapId={mapId}
-                layer={layer}
-                className="test"
-                data-testid="overview-map"
-            ></OverviewMap>
-        </PackageContextProvider>
-    );
-
-    await waitForOverviewMap();
-    const controls = map.olMap.getControls().getArray();
-    const overViewMapControl = controls.find((control) => control instanceof OlOverviewMap) as
-        | OlOverviewMap
-        | undefined;
-    const ogcWMSLayer: VectorLayer<VectorSource> = overViewMapControl
-        ?.getOverviewMap()
-        ?.getLayers()
-        .getArray()[0] as VectorLayer<VectorSource>;
-    const source = ogcWMSLayer?.getSource();
-
-    expect(ogcWMSLayer).toBeInstanceOf(ImageLayer);
-    expect(source).toBeInstanceOf(ImageWMS);
 });
 
 async function waitForOverviewMap() {
@@ -220,6 +117,13 @@ async function waitForOverviewMap() {
 
     return { overviewMapDiv };
 }
+
+function getControl(olMap: OlMap) {
+    const controls = olMap.getControls().getArray();
+    return controls.find((control) => control instanceof OlOverviewMap) as
+        | OlOverviewMap
+        | undefined;
+}
 function getTileLayer() {
     return new TileLayer({
         source: new OSM()
@@ -229,34 +133,5 @@ function getTileLayer() {
 function getTileLayerOfWMTS() {
     return new TileLayer({
         source: new BkgTopPlusOpen()
-    });
-}
-
-function getOGCAPIFeatureLayer() {
-    return new VectorLayer({
-        source: createVectorSource({
-            baseUrl: "https://ogc-api.nrw.de/inspire-us-kindergarten/v1",
-            collectionId: "governmentalservice",
-            crs: "http://www.opengis.net/def/crs/EPSG/0/3857",
-            attributions:
-                "<a href='https://www.govdata.de/dl-de/by-2-0'>Datenlizenz Deutschland - Namensnennung - Version 2.0</a>"
-        })
-    });
-}
-
-function getOGCAPIFeatureLayerWithMapboxStyle() {
-    return new MapboxVectorLayer({
-        styleUrl: "https://demo.ldproxy.net/strassen/styles/default?f=mbs",
-        accessToken: null
-    });
-}
-
-function getOGCWMSLayer() {
-    return new ImageLayer({
-        source: new ImageWMS({
-            url: "https://www.wms.nrw.de/wms/wms_nw_inspire-schulen",
-            params: { "LAYERS": ["US.education"] },
-            ratio: 1 //Ratio. 1 means image requests are the size of the map viewport
-        })
     });
 }
