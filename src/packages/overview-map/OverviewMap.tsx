987b64cf317a1370c01d8baba6249157aef8d644
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-11-09 16:14:21 +0100

    Support custom overview map size via props

--- src/packages/overview-map/OverviewMap.tsx
+++ src/packages/overview-map/OverviewMap.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import { FC, useEffect, useRef } from "react";
 import { useMapModel } from "@open-pioneer/map";
-import { Box } from "@open-pioneer/chakra-integration";
+import { Box, BoxProps } from "@open-pioneer/chakra-integration";
 import { OverviewMap as OlOverviewMap } from "ol/control";
 import { CommonComponentProps, useCommonComponentProps } from "@open-pioneer/react-utils";
 import OlBaseLayer from "ol/layer/Base";
@@ -20,13 +20,28 @@ export interface OverviewMapProps extends CommonComponentProps {
      * The layer shown in the overview map.
      */
     olLayer: OlBaseLayer;
+
+    /**
+     * The height of the map.
+     * This defaults to a reasonable pixel size.
+     */
+    height?: BoxProps["height"];
+
+    /**
+     * The width of the map.
+     * This defaults to a reasonable pixel size.
+     */
+    width?: BoxProps["width"];
 }
+
+const DEFAULT_HEIGHT = "200px";
+const DEFAULT_WIDTH = "300px";
 
 /**
  * The `OverviewMap` component can be used in an app to have a better overview of the current location in the map.
  */
 export const OverviewMap: FC<OverviewMapProps> = (props) => {
-    const { mapId, olLayer } = props;
+    const { mapId, olLayer, height = DEFAULT_HEIGHT, width = DEFAULT_WIDTH } = props;
     const { containerProps } = useCommonComponentProps("overview-map", props);
     const overviewMapControlElem = useRef(null);
     const { map } = useMapModel(mapId);
@@ -48,5 +63,7 @@ export const OverviewMap: FC<OverviewMapProps> = (props) => {
         }
     }, [map, olLayer]);
 
-    return <Box ref={overviewMapControlElem} {...containerProps}></Box>;
+    return (
+        <Box height={height} width={width} ref={overviewMapControlElem} {...containerProps}></Box>
+    );
 };
