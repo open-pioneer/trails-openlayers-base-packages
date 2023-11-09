ce21f2486b5054f13dd00383a5654eff028fde83
Marcel Simon <129262172+simon05111@users.noreply.github.com>
2023-11-09 16:06:06 +0100

    Add theme package and use it in samples

--- src/samples/theming-sample/theming-app/app.ts
+++ src/samples/theming-sample/theming-app/app.ts
@@ -1,9 +1,9 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { createCustomElement } from "@open-pioneer/runtime";
+import { theme } from "@open-pioneer/theme";
 import * as appMetadata from "open-pioneer:app";
 import { AppUI } from "./AppUI";
-import { theme } from "./theme/theme";
 
 const Element = createCustomElement({
     component: AppUI,
