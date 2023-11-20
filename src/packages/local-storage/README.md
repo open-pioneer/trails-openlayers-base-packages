70aa351ff4a97876656de6b5564f10d4317925ea
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-11-20 10:04:24 +0100

    init local storage package

---
+++ src/packages/local-storage/README.md
@@ -0,0 +1,7 @@
+# @open-pioneer/local-storage
+
+TODO
+
+## License
+
+Apache-2.0 (see `LICENSE` file)
---
+++ src/packages/local-storage/index.ts
@@ -0,0 +1,2 @@
+// SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-License-Identifier: Apache-2.0
