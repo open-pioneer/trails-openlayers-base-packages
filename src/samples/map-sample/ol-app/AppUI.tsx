319baba25044aee88fbf0bb02d7350fb26dd4dc8
Bierett, Danilo <danilo.bierett@it.nrw.de>
2023-11-24 14:44:55 +0100

    Add maxWidth to box which includes toc, measurement

--- src/samples/map-sample/ol-app/AppUI.tsx
+++ src/samples/map-sample/ol-app/AppUI.tsx
@@ -78,6 +78,7 @@ export function AppUI() {
                                     borderRadius="lg"
                                     padding={2}
                                     boxShadow="lg"
+                                    maxWidth={350}
                                 >
                                     {showToc && (
                                         <Box role="dialog" aria-labelledby={tocTitleId}>
