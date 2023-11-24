67572031e7850d6045ee1de68eb802e289f908af
Senait <s.alema@conterra.de>
2023-11-24 11:25:28 +0100

    add test for adding buffer to extent and configured zoom levels

--- src/packages/search-result-handler/ResultHandler.test.ts
+++ src/packages/search-result-handler/ResultHandler.test.ts
@@ -3,8 +3,9 @@
 
 import { expect, it } from "vitest";
 import OlMap from "ol/Map";
-import { removerHighlight, resultHandler } from "./ResultHandler";
+import { removerHighlight, resultHandler, calculateBufferedExtent } from "./ResultHandler";
 import { LineString, Point, Polygon } from "ol/geom";
+import { containsExtent } from "ol/extent";
 
 it("should successfully zoom and add marker for point geometries", async () => {
     const map = new OlMap();
@@ -95,4 +96,49 @@ it("should successfully remove all markers or highlights", async () => {
         .getArray()
         .find((l) => l.getClassName().includes("search_result_layer"));
     expect(layerAfterRemove).toBeUndefined();
+});
+
+it("should calculate a buffered extent of a given extent", async () => {
+    const extent = [844399.851466, 6788384.425292, 852182.096409, 6794764.528497];
+    const bufferedExtent = calculateBufferedExtent(extent);
+
+    expect(bufferedExtent).toBeDefined();
+
+    bufferedExtent && expect(containsExtent(bufferedExtent, extent)).toBe(true);
+});
+
+it("should zoom the map to the extent of the geometries but not further than the defined maxZoom", async () => {
+    const map = new OlMap();
+
+    const line = new LineString([
+        [848107.047338, 6790579.601198],
+        [849081.619449, 6793197.569417]
+    ]);
+
+    resultHandler(map, [line], {});
+    const mapZoom = map.getView().getZoom();
+
+    //default maxZoom is 20
+    expect(mapZoom).toBeLessThanOrEqual(20);
+
+    resultHandler(map, [line], { maxZoom: 13 });
+    const mapZoom2 = map.getView().getZoom();
+
+    expect(mapZoom2).toBeLessThanOrEqual(13);
+});
+
+it("should zoom the map to the default or configured zoom level if there is no extent", async () => {
+    const map = new OlMap();
+
+    const point = new Point([852011.307424, 6788511.322702]);
+
+    resultHandler(map, [point], {});
+    const defaultZoom = map.getView().getZoom();
+
+    expect(defaultZoom).toStrictEqual(17);
+
+    resultHandler(map, [point], { zoom: 12 });
+    const configuredZoom = map.getView().getZoom();
+
+    expect(configuredZoom).toStrictEqual(12);
 });
--- src/packages/search-result-handler/index.ts
+++ src/packages/search-result-handler/index.ts
@@ -1,4 +1,9 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 
-export { resultHandler, type ResultHandlerOptions, removerHighlight } from "./ResultHandler";
+export {
+    resultHandler,
+    type ResultHandlerOptions,
+    removerHighlight,
+    calculateBufferedExtent
+} from "./ResultHandler";
