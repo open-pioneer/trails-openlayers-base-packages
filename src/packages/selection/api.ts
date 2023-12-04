6accef472273e7596f0dc5dd9df126c07550535c
Sven Reissig <s.reissig@conterra.de>
2023-12-04 12:10:31 +0100

    Init Interfaces

---
+++ src/packages/selection/api.ts
@@ -0,0 +1,81 @@
+// SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-License-Identifier: Apache-2.0
+import { Geometry } from "ol/geom";
+import { Projection } from "ol/proj";
+import { Extent } from "ol/extent";
+
+export type SelectionSourceStatus = "available" | "unavailable" | (string & {});
+
+export interface SelectionResult {
+    /* Identifier for the result object.
+     * Values used here should be unique within the context of the selection source that returns them.
+     *
+     * If your source cannot provide a useful id on its own, another strategy to generate unique ids is to
+     * generate a [UUID](https://www.npmjs.com/package/uuid#uuidv4options-buffer-offset) instead.
+     */
+    id: number | string;
+
+    /**
+     * Geometry of the selection result.
+     * One should also specify the {@link projection}.
+     */
+    geometry: Geometry;
+
+    /**
+     * The projection of the {@link geometry}.
+     */
+    projection?: string;
+
+    /**
+     * Arbitrary additional properties.
+     */
+    properties?: Readonly<Record<string, unknown>>;
+}
+
+/** Options passed to a {@link SelectionSource} when triggering a select. */
+export interface SelectionOptions {
+    /**
+     * The maximum number of selection results to request.
+     * This property allows the source to fetch no more results than necessary.
+     */
+    maxResults: number;
+
+    /**
+     * The current projection of the map.
+     * Useful to return the selection result's geometry in the suitable projection, should they differ.
+     */
+    mapProjection: Projection;
+}
+
+/**
+ * An object that allows spatial selection.
+ *
+ * Developers can create classes that implement this interface for different selection sources.
+ */
+export interface SelectionSource {
+    /**
+     * The label of this source.
+     *
+     * This will be displayed by the user interface when TODO
+     */
+    readonly label: string;
+
+    /**
+     * The optional status of this source. If there is no status defined, it is assumed that the
+     * source is always available.
+     *
+     * This will be displayed by the user interface.
+     */
+    readonly status?: SelectionSourceStatus;
+
+    /**
+     * Performs a selection and returns a list of selection results.
+     *
+     * Implementations should return the results ordered by priority (best match first), if possible.
+     *
+     * TODO: Reconsider name?
+     * @param selectionKind: The geometry with which to perform the spatial selection. Currently only
+     * an extent is supported.
+     */
+    select(selectionKind: Extent, options: SelectionOptions): Promise<SelectionResult[]>;
+}
--- src/packages/selection/index.ts
+++ src/packages/selection/index.ts
@@ -1,3 +1,9 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 export { Selection } from "./Selection";
+export type {
+    SelectionSource,
+    SelectionSourceStatus,
+    SelectionResult,
+    SelectionOptions
+} from "./api";
