305b23437a3d54e407b5d4fbee4aface911ea63f
Sven Reissig <s.reissig@conterra.de>
2023-12-05 08:33:35 +0100

    removed readonly status

--- src/packages/selection/api.ts
+++ src/packages/selection/api.ts
@@ -66,7 +66,7 @@ export interface SelectionSource {
      *
      * This will be displayed by the user interface.
      */
-    readonly status?: SelectionSourceStatus;
+    status?: SelectionSourceStatus;
 
     /**
      * Performs a selection and returns a list of selection results.
