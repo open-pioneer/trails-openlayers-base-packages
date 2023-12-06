bfb1fd7a0e9e2370bc6902ce0c8c4b42e6a8e427
m.eisenblaetter <m.eisenblaetter@conterra.de>
2023-12-06 12:27:36 +0100

    Add react-icons to package.json, remove unused import

--- src/packages/selection/Selection.tsx
+++ src/packages/selection/Selection.tsx
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import {
-    Box,
     Button,
     FormControl,
     FormLabel,
