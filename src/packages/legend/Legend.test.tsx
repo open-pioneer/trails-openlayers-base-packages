484bb3cb7bc0c3975353cbd70825f49611c38348
Antonia van Eek <a.vaneek@conterra.de>
2023-12-12 11:32:53 +0100

    implemented more tests

--- src/packages/legend/Legend.test.tsx
+++ src/packages/legend/Legend.test.tsx
@@ -6,9 +6,11 @@ import { createServiceOptions, setupMap } from "@open-pioneer/map-test-utils";
 import TileLayer from "ol/layer/Tile";
 import { render, screen, waitFor } from "@testing-library/react";
 import { PackageContextProvider } from "@open-pioneer/test-utils/react";
-import { Legend } from "./Legend";
+import { Legend, LegendItemComponentProps } from "./Legend";
+import { Box, Text } from "@open-pioneer/chakra-integration";
 
 const LEGEND_ITEM_CLASS = ".legend-item";
+const LEGEND_IMAGE_CLASS = ".legend-item__image";
 
 it("should successfully create a legend component", async () => {
     const { mapId, registry } = await setupMap({
@@ -81,7 +83,148 @@ it("should successfully show legend for imageUrl configuration", async () => {
 
     const legendDiv = await findLegend();
     await waitForLegendItem(legendDiv);
+    // do snapshot test to test if layer title and image are shown correctly
     expect(legendDiv).toMatchSnapshot();
+});
+
+it("should successfully show legend for Component configuration", async () => {
+    const dotStyle = {
+        height: "25px",
+        width: "25px",
+        borderColor: "#4cb3ff",
+        borderWidth: "3px",
+        borderRadius: "50%",
+        display: "inline-block "
+    };
+
+    const { mapId, registry } = await setupMap({
+        layers: [
+            {
+                title: "Base layer",
+                id: "base-layer",
+                olLayer: new TileLayer({}),
+                isBaseLayer: true
+            },
+            {
+                title: "Layer 1",
+                id: "layer-1",
+                olLayer: new TileLayer({}),
+                attributes: {
+                    "legend": {
+                        Component: function CustomLegend(props: LegendItemComponentProps) {
+                            return (
+                                <Box>
+                                    <Text>{props.layer.title}</Text>
+                                    <Box style={dotStyle}></Box>
+                                </Box>
+                            );
+                        }
+                    }
+                }
+            },
+            {
+                title: "Layer 2",
+                id: "layer-2",
+                olLayer: new TileLayer({})
+            }
+        ]
+    });
+    await registry.expectMapModel(mapId);
+    const injectedServices = createServiceOptions({ registry });
+
+    render(
+        <PackageContextProvider services={injectedServices}>
+            <Legend mapId={mapId} data-testid="legend" />
+        </PackageContextProvider>
+    );
+
+    const legendDiv = await findLegend();
+    await waitForLegendItem(legendDiv);
+    expect(legendDiv).toMatchSnapshot();
+});
+
+it("does not show a legend for basemaps by default", async () => {
+    const { mapId, registry } = await setupMap({
+        layers: [
+            {
+                title: "Base layer",
+                id: "base-layer",
+                olLayer: new TileLayer({}),
+                isBaseLayer: true,
+                attributes: {
+                    "legend": {
+                        imageUrl: "https://basemap-url.com/"
+                    }
+                }
+            },
+            {
+                title: "Layer 1",
+                id: "layer-1",
+                olLayer: new TileLayer({}),
+                attributes: {
+                    "legend": {
+                        imageUrl: "https://avatars.githubusercontent.com/u/121286957?s=200&v=4"
+                    }
+                }
+            },
+            {
+                title: "Layer 2",
+                id: "layer-2",
+                olLayer: new TileLayer({})
+            }
+        ]
+    });
+    await registry.expectMapModel(mapId);
+    const injectedServices = createServiceOptions({ registry });
+
+    render(
+        <PackageContextProvider services={injectedServices}>
+            <Legend mapId={mapId} data-testid="legend" />
+        </PackageContextProvider>
+    );
+
+    const legendDiv = await findLegend();
+    await waitForLegendItem(legendDiv);
+    const images = await getLegendImages(legendDiv);
+    expect(images.length).toBe(1);
+    const src = images[0]?.getAttribute("src");
+    expect(src).not.toBe("https://basemap-url.com/");
+});
+
+it("shows a legend for active basemap if showBaseLayers is configured to be true", async () => {
+    // TODO
+});
+
+it("shows legend entries for nested sublayers", async () => {
+    // TODO
+});
+
+it("shows legend entries in correct order", async () => {
+    // TODO
+});
+
+it("shows legend entries only for visible layers", async () => {
+    // TODO
+});
+
+it("includes the layer id in the legend item's class list", async () => {
+    // TODO
+});
+
+it("shows an empty box if not legend entries are available", async () => {
+    // TODO
+});
+
+it("reacts to changes in layer visibility", async () => {
+    // TODO
+});
+
+it("reacts to changes in layer legend attributes", async () => {
+    // TODO
+});
+
+it("reacts to changes in the layer composition", async () => {
+    // TODO
 });
 
 async function findLegend() {
@@ -93,9 +236,20 @@ async function waitForLegendItem(legendDiv: HTMLElement) {
     return await waitFor(() => {
         const legendItem = legendDiv.querySelector(LEGEND_ITEM_CLASS);
         if (!legendItem) {
-            throw new Error("basemap switcher not mounted");
+            throw new Error("legend item not mounted");
         }
 
         return legendItem;
+    });
+}
+
+async function getLegendImages(legendDiv: HTMLElement) {
+    return await waitFor(() => {
+        const legendImages = legendDiv.querySelectorAll(LEGEND_IMAGE_CLASS);
+        if (!legendImages) {
+            throw new Error("legend images not mounted");
+        }
+
+        return legendImages;
     });
 }
