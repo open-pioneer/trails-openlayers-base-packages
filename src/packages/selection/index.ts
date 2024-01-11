fe1cd6e5f30fe37b43b1fd1b1a898b1750846ea0
Bierett, Danilo <danilo.bierett@it.nrw.de>
2024-01-11 08:42:36 +0100

    Fix documentation

--- src/packages/selection/index.ts
+++ src/packages/selection/index.ts
@@ -1,4 +1,9 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 export * from "./api";
-export { Selection, type SelectionProps } from "./Selection";
+export {
+    Selection,
+    type SelectionProps,
+    type SelectionCompleteEvent,
+    type SelectionSourceChangedEvent
+} from "./Selection";
