ce89115822d1aa2459eb417019153e23ac234ccd
Senait <s.alema@conterra.de>
2024-01-22 14:23:42 +0100

    default value for showBaseLayers should be false

--- src/samples/map-sample/ol-app/AppUI.tsx
+++ src/samples/map-sample/ol-app/AppUI.tsx
@@ -195,7 +195,7 @@ export function AppUI() {
                                                 }
                                             >
                                                 <Box overflowY="auto" maxHeight={300}>
-                                                    <Legend mapId={MAP_ID} showBaseLayers={true} />
+                                                    <Legend mapId={MAP_ID} showBaseLayers={false} />
                                                 </Box>
                                             </TitledSection>
                                         </Box>
