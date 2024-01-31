6ae0ac28f0930cb81e9bc275c1fc97b7a949b6ba
Bierett, Danilo <danilo.bierett@it.nrw.de>
2024-01-31 08:07:17 +0100

    Use conditional operator

--- src/samples/map-sample/ol-app/util/map-utils.ts
+++ src/samples/map-sample/ol-app/util/map-utils.ts
@@ -1,14 +1,15 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-import { MapModel, MapPadding } from "@open-pioneer/map";
+import { MapModel } from "@open-pioneer/map";
 import { Geometry } from "ol/geom";
 
 export function highlightAndZoom(map: MapModel, geometry: Geometry[]) {
-    let viewPadding: MapPadding = { top: 150, right: 400, bottom: 50, left: 400 };
+    const viewport: HTMLElement = map.olMap.getViewport();
 
-    if (map.olMap.getViewport().offsetWidth < 1000) {
-        viewPadding = { top: 150, right: 75, bottom: 50, left: 75 };
-    }
-
-    map.highlightAndZoom(geometry, { viewPadding });
+    map.highlightAndZoom(geometry, {
+        viewPadding:
+            viewport && viewport.offsetWidth < 1000
+                ? { top: 150, right: 75, bottom: 50, left: 75 }
+                : { top: 150, right: 400, bottom: 50, left: 400 }
+    });
 }
