7607c1df8f9c347d75265487810639ab69c62d95
Senait <s.alema@conterra.de>
2024-02-15 10:54:11 +0100

    update UI-test

--- src/packages/printing/Printing.test.tsx
+++ src/packages/printing/Printing.test.tsx
@@ -3,10 +3,11 @@
 
 import { expect, it } from "vitest";
 import { createServiceOptions, setupMap } from "@open-pioneer/map-test-utils";
-import { render, screen, waitFor } from "@testing-library/react";
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { PackageContextProvider } from "@open-pioneer/test-utils/react";
 import { Printing } from "./Printing";
 import { NotificationService } from "@open-pioneer/notifier";
+import userEvent from "@testing-library/user-event";
 
 it("should successfully create a printing component", async () => {
     await createPrinting();
@@ -18,6 +19,21 @@ it("should successfully create a printing component", async () => {
     // check printing input and select are available
     expect(printingInput.tagName).toBe("INPUT");
     expect(printingSelect.tagName).toBe("SELECT");
+});
+
+it("should successfully type into title input field and select file format", async () => {
+    const user = userEvent.setup();
+    await createPrinting();
+
+    const { printingInput, printingSelect } = await waitForPrinting();
+
+    await user.type(printingInput, "test title");
+    expect(printingInput).toHaveValue("test title");
+
+    act(() => {
+        fireEvent.change(printingSelect, { target: { value: "pdf" } });
+    });
+    expect(printingSelect.value).toBe("pdf");
 });
 
 async function createPrinting() {
