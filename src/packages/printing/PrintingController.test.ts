9f0619eb2204170d5798cef22a8b36e042dda187
Michael Beckemeyer <m.beckemeyer@conterra.de>
2024-02-15 14:33:04 +0100

    Add test for overlay

--- src/packages/printing/PrintingController.test.ts
+++ src/packages/printing/PrintingController.test.ts
@@ -1,15 +1,62 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 
-import { vi, expect, it } from "vitest";
+import { vi, it, expect } from "vitest";
 import { PrintingController } from "./PrintingController";
 import OlMap from "ol/Map";
+import { createManualPromise } from "@open-pioneer/core";
 
-it("should add overlay while printing and remove it after printing", async () => {
-    const { olMap, controller } = setUp();
+it("calls the appropriate methods happy path)", async () => {
+    const { olMap, controller, exportMapInPNGSpy} = setUp();
 
     await controller.handleMapExport();
+    // TODO: spy...
 });
+
+it("creates an overlay during export", async () => {
+    const { olMap, exportToCanvasSpy, controller } = setUp();
+
+    // Block the canvas export until we tell it to continue.
+    // This way, we can test the intermediate state (overlay present etc.)
+    // Note that this is pretty ugly - it relies on implementation details.
+    const { promise, resolve } = createManualPromise();
+    exportToCanvasSpy.mockImplementation(() => promise);
+
+    // Start the export, but do _not_ wait for it to finish.
+    const exportDonePromise = controller.handleMapExport();
+
+    // Wait for the overlay to appear.
+    const overlay = await vi.waitFor(() => {
+        const overlay = findOverlay(olMap);
+        if (!overlay) {
+            throw new Error("Failed to find overlay");
+        }
+        return overlay;
+    });
+    expect(overlay.textContent).toBe("Map printing ...");
+
+    // Continue from canvas export
+    resolve(document.createElement("canvas"));
+
+    // Wait for the method to complete
+    await exportDonePromise;
+
+    // Overlay has been removed
+    expect(findOverlay(olMap)).toBe(undefined);
+});
+
+function findOverlay(olMap: OlMap) {
+    const target = olMap.getTarget() as HTMLElement | undefined;
+    if (!target) {
+        throw new Error("target not present");
+    }
+
+    const overlay = target.querySelector("> .printing-overlay");
+    if (!overlay) {
+        return undefined;
+    }
+    return overlay;
+}
 
 function setUp() {
     const olMap = new OlMap();
