906a8402c2f115b3017264f1e31a601e8b1da547
Michael Beckemeyer <m.beckemeyer@conterra.de>
2024-02-18 20:44:57 +0100

    Fix import of a private module of another package

--- src/packages/result-list/DataTable/SelectCheckbox.tsx
+++ src/packages/result-list/DataTable/SelectCheckbox.tsx
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { Checkbox, Tooltip } from "@open-pioneer/chakra-integration";
-import { ChangeEvent, useRef } from "react";
+import { ChangeEvent } from "react";
 
 export interface SelectCheckboxProps {
     className?: string;
