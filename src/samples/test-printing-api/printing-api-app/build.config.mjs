a2a6c632dbe4c1386a05f54327cc4b2e0d1c5484
SENAIT MELES ALEMA <s.alema@conterra.de>
2024-03-12 16:09:59 +0100

    Printing API (#271, #293)

--- src/samples/test-printing-api/printing-api-app/build.config.mjs
+++ src/samples/test-printing-api/printing-api-app/build.config.mjs
@@ -1,21 +1,15 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-
 import { defineBuildConfig } from "@open-pioneer/build-support";
 
 export default defineBuildConfig({
-    entryPoints: ["index"],
     i18n: ["en", "de"],
-    styles: "./styles.css",
     services: {
-        PrintingServiceImpl: {
-            provides: "printing.PrintingService"
+        MapConfigProviderImpl: {
+            provides: ["map.MapConfigProvider"]
         }
     },
     ui: {
-        references: ["notifier.NotificationService", "printing.PrintingService"]
-    },
-    publishConfig: {
-        strict: true
+        references: ["printing.PrintingService"]
     }
 });
