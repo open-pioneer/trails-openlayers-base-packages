64afee2eed428f1c71e56de83b30689ea078bacb
Michael Beckemeyer <m.beckemeyer@conterra.de>
2024-05-10 11:33:13 +0200

    add package metadata

--- src/packages/authentication-keycloak/build.config.mjs
+++ src/packages/authentication-keycloak/build.config.mjs
@@ -3,6 +3,7 @@
 import { defineBuildConfig } from "@open-pioneer/build-support";
 
 export default defineBuildConfig({
+    entryPoints: ["index.ts"],
     services: {
         KeycloakAuthPlugin: {
             provides: ["authentication.AuthPlugin"]
