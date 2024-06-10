4392d3dd3e08e01d4839e637a3db51d75ff08477
Michael Beckemeyer <m.beckemeyer@conterra.de>
2024-06-10 13:26:13 +0200

    Add new app

---
+++ src/samples/showcase/showcase-app/build.config.mjs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-License-Identifier: Apache-2.0
+import { defineBuildConfig } from "@open-pioneer/build-support";
+
+export default defineBuildConfig({
+    i18n: ["en", "de"],
+    services: {},
+    ui: {
+        references: []
+    }
+});
