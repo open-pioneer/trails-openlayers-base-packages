594143eb53392ed98f0a572ee700b9e1478a2f32
Michael Beckemeyer <m.beckemeyer@conterra.de>
2024-07-25 15:49:12 +0200

    Simplify sample app (#301, #329)
    Co-authored-by: Antonia van Eek <a.vaneek@conterra.de>

--- src/samples/map-sample/ol-app/app.ts
+++ src/samples/map-sample/ol-app/app.ts
@@ -1,27 +1,23 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { createCustomElement } from "@open-pioneer/runtime";
-import { theme } from "@open-pioneer/theme";
 import * as appMetadata from "open-pioneer:app";
 import { AppUI } from "./ui/AppUI";
 
+// This reads the `lang` parameter from the URL and uses it for the application's locale.
+const URL_PARAMS = new URLSearchParams(window.location.search);
+const FORCED_LANG = URL_PARAMS.get("lang") || undefined;
+
 const element = createCustomElement({
     component: AppUI,
-    theme,
     appMetadata,
     config: {
+        locale: FORCED_LANG,
         properties: {
             "@open-pioneer/local-storage": {
                 "storageId": "ol-app-state"
             }
         }
-    },
-    async resolveConfig(ctx) {
-        const locale = ctx.getAttribute("forced-locale");
-        if (!locale) {
-            return undefined;
-        }
-        return { locale };
     }
 });
 
