50eee7db994834e33de685cafc0e9904c22f7713
Michael Beckemeyer <m.beckemeyer@conterra.de>
2024-08-15 12:55:40 +0200

    Remove unneeded promise

--- src/packages/geolocation/GeolocationController.ts
+++ src/packages/geolocation/GeolocationController.ts
@@ -86,7 +86,7 @@ export class GeolocationController {
 
     startGeolocation() {
         if (this.#active.value) {
-            return Promise.resolve();
+            return;
         }
 
         const olMap = this.olMap;
