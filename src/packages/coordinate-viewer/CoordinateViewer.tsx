c4e519a64262b92042003d84b0592f93d02d1ba9
t.krumrein <t.krumrein@conterra.de>
2024-09-12 14:57:14 +0200

    Added degree option to coordinate viewer

--- src/packages/coordinate-viewer/CoordinateViewer.tsx
+++ src/packages/coordinate-viewer/CoordinateViewer.tsx
@@ -13,6 +13,7 @@ import { useIntl } from "open-pioneer:react-hooks";
 import { FC, useEffect, useState } from "react";
 
 const DEFAULT_PRECISION = 4;
+const DEFAULT_DISPLAY_DEGREE = false;
 
 /**
  * These are special properties for the CoordinateViewer.
@@ -32,13 +33,18 @@ export interface CoordinateViewerProps extends CommonComponentProps {
      * Projection of the coordinates shown in the rendered HTML, does not affect the map projection
      */
     displayProjectionCode?: string;
+
+    /**
+     * Projection of the coordinates shown in the rendered HTML as Degree Format, does not affect the map projection
+     */
+    displayProjectionAsDegree?: boolean;
 }
 
 /**
  * The `CoordinateViewer`component can be used in an app to render the coordinates at the current mouse position.
  */
 export const CoordinateViewer: FC<CoordinateViewerProps> = (props) => {
-    const { mapId, precision, displayProjectionCode } = props;
+    const { mapId, precision, displayProjectionCode, displayProjectionAsDegree } = props;
     const { containerProps } = useCommonComponentProps("coordinate-viewer", props);
     const { map } = useMapModel(mapId);
     const olMap = map?.olMap;
@@ -48,7 +54,11 @@ export const CoordinateViewer: FC<CoordinateViewerProps> = (props) => {
         coordinates && displayProjectionCode
             ? transformCoordinates(coordinates, mapProjectionCode, displayProjectionCode)
             : coordinates;
-    const coordinatesString = useCoordinatesString(coordinates, precision);
+    const coordinatesString = useCoordinatesString(
+        coordinates,
+        precision,
+        displayProjectionAsDegree
+    );
     const projectionString = displayProjectionCode ? displayProjectionCode : mapProjectionCode;
     const displayString = coordinatesString ? coordinatesString + " " + projectionString : "";
     return (
@@ -61,10 +71,13 @@ export const CoordinateViewer: FC<CoordinateViewerProps> = (props) => {
 /* Separate function for easier testing */
 export function useCoordinatesString(
     coordinates: number[] | undefined,
-    precision: number | undefined
+    precision: number | undefined,
+    displayProjectionAsDegree: boolean | undefined
 ): string {
     const intl = useIntl();
-    const coordinatesString = coordinates ? formatCoordinates(coordinates, precision, intl) : "";
+    const coordinatesString = coordinates
+        ? formatCoordinates(coordinates, precision, intl, displayProjectionAsDegree)
+        : "";
     return coordinatesString;
 }
 
@@ -89,26 +102,59 @@ function useCoordinates(map: OlMap | undefined): { coordinates: Coordinate | und
 function formatCoordinates(
     coordinates: number[],
     configuredPrecision: number | undefined,
-    intl: PackageIntl
+    intl: PackageIntl,
+    displayProjectionAsDegree: boolean | undefined
 ) {
     if (coordinates[0] == null || coordinates[1] == null) {
         return "";
     }
 
     const precision = configuredPrecision ?? DEFAULT_PRECISION;
+    const asDegree = displayProjectionAsDegree ?? DEFAULT_DISPLAY_DEGREE;
     const [x, y] = coordinates;
 
-    const xString = intl.formatNumber(x, {
-        maximumFractionDigits: precision,
-        minimumFractionDigits: precision
-    });
-    const yString = intl.formatNumber(y, {
+    if (asDegree && isFinite(x) && isFinite(y)) {
+        const [xHour, xMin, xSek] = toDegree(x, intl, precision);
+        const [yHour, yMin, ySek] = toDegree(y, intl, precision);
+
+        const xString = `${Math.abs(xHour)}°${xMin}'${xSek}"${0 <= xHour ? "(E)" : "(W)"}`;
+        const yString = `${Math.abs(yHour)}°${yMin}'${ySek}"${0 <= yHour ? "(N)" : "(S)"}`;
+
+        const coordinatesString = xString + " " + yString;
+        return coordinatesString;
+    } else {
+        const xString = intl.formatNumber(x, {
+            maximumFractionDigits: precision,
+            minimumFractionDigits: precision
+        });
+        const yString = intl.formatNumber(y, {
+            maximumFractionDigits: precision,
+            minimumFractionDigits: precision
+        });
+        const coordinatesString = xString + " " + yString;
+        return coordinatesString;
+    }
+}
+
+function toDegree(
+    coordPart: number,
+    intl: PackageIntl,
+    precision: number
+): [number, number, string] {
+    const cHour = Math.floor(coordPart);
+    const cNach = coordPart - cHour;
+
+    const cMin = Math.floor(60 * cNach);
+
+    const cMinNach = 60 * cNach - cMin;
+
+    const cSek = 60 * cMinNach;
+    const cSekRounded = intl.formatNumber(cSek, {
         maximumFractionDigits: precision,
         minimumFractionDigits: precision
     });
 
-    const coordinatesString = xString + " " + yString;
-    return coordinatesString;
+    return [cHour, cMin, cSekRounded];
 }
 
 function transformCoordinates(
