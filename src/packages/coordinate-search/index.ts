9bf437cde2e75b2b63ee38dd388c2b1f62595eaf
Matheisen, Alexander <alexander.matheisen@it.nrw.de>
2024-10-21 19:04:05 +0200

    prettier

--- src/packages/coordinate-search/index.ts
+++ src/packages/coordinate-search/index.ts
@@ -1,3 +1,7 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-export { CoordinateSearch, type CoordinateSearchProps, type CoordsSelectEvent } from "./CoordinateSearch";
+export {
+    CoordinateSearch,
+    type CoordinateSearchProps,
+    type CoordsSelectEvent
+} from "./CoordinateSearch";
