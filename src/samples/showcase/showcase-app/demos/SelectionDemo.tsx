d6ebd772cbb474114c08b296482c091df1bca19c
Michael Beckemeyer <m.beckemeyer@conterra.de>
2025-02-20 15:48:10 +0100

    Update license headers

--- src/samples/showcase/showcase-app/demos/SelectionDemo.tsx
+++ src/samples/showcase/showcase-app/demos/SelectionDemo.tsx
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { reactive } from "@conterra/reactivity-core";
 import { BaseFeature, Layer, MapModel, SimpleLayer } from "@open-pioneer/map";
