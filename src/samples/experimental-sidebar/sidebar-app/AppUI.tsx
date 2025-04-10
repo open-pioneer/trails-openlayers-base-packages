1ff04598eace730179e4484d8ec089570d47f3d7
Antonia van Eek <a.vaneek@conterra.de>
2025-04-10 16:52:51 +0200

    migrate experimental-sidebar app

--- src/samples/experimental-sidebar/sidebar-app/AppUI.tsx
+++ src/samples/experimental-sidebar/sidebar-app/AppUI.tsx
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-import { Box, Button, Flex } from "@open-pioneer/chakra-integration";
+import { Box, Button, Flex } from "@chakra-ui/react";
 import { Sidebar, SidebarItem } from "@open-pioneer/experimental-layout-sidebar";
 import {
     DefaultMapProvider,
