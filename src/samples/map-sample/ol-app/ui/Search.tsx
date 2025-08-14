04c9959d1f1f6fe20e73547ceb59476b86cb2c3c
Michael Beckemeyer <m.beckemeyer@conterra.de>
2025-08-14 11:14:28 +0200

    Update ol-app to use useMapModelValue where appropriate

--- src/samples/map-sample/ol-app/ui/Search.tsx
+++ src/samples/map-sample/ol-app/ui/Search.tsx
@@ -1,23 +1,20 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { Box } from "@chakra-ui/react";
-import { useMapModel } from "@open-pioneer/map";
+import { useMapModelValue } from "@open-pioneer/map";
+import { useReactiveSnapshot } from "@open-pioneer/reactivity";
 import { Search, SearchSelectEvent } from "@open-pioneer/search";
 import { useIntl, useService } from "open-pioneer:react-hooks";
 import { AppModel } from "../AppModel";
-import { useReactiveSnapshot } from "@open-pioneer/reactivity";
 
 export function SearchComponent() {
     const intl = useIntl();
-    const { map } = useMapModel(); // uses default map configured in AppUI.tsx
+    const map = useMapModelValue(); // uses default map configured in AppUI.tsx
     const appModel = useService<AppModel>("ol-app.AppModel");
     const sources = useReactiveSnapshot(() => appModel.searchSources.getItems(), [appModel]);
 
     function onSearchResultSelected(event: SearchSelectEvent) {
         console.debug("The user selected the following item: ", event.result);
-        if (!map) {
-            return;
-        }
 
         const geometry = event.result.geometry;
         if (!geometry) {
