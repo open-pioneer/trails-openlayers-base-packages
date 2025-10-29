a823f9da64a07fbd377e8f108d84ace15ac7e014
Matheisen, Alexander <alexander.matheisen@it.nrw.de>
2025-10-29 17:17:51 +0100

    replaces resolution by zoom in mapconfig for better readability, add example for group layer to demonstrate inheritance

--- src/samples/test-toc/toc-app/MapConfigProviderImpl.ts
+++ src/samples/test-toc/toc-app/MapConfigProviderImpl.ts
@@ -50,8 +50,8 @@ export class MapConfigProviderImpl implements MapConfigProvider {
                     title: "Orthofotos NRW",
                     url: "https://www.wmts.nrw.de/geobasis/wmts_nw_dop/1.0.0/WMTSCapabilities.xml",
                     name: "nw_dop",
-                    minResolution: 300,
-                    maxResolution: 500,
+                    minZoom: 10,
+                    maxZoom: 16,
                     matrixSet: "EPSG_3857_16",
                     sourceOptions: {
                         attributions: `Die Geobasisdaten des amtlichen Vermessungswesens werden als öffentliche Aufgabe gem. VermKatG NRW und gebührenfrei nach Open Data-Prinzipien über online-Verfahren bereitgestellt. Nutzungsbedingungen: siehe <a href="https://www.bezreg-koeln.nrw.de/system/files/media/document/file/lizenzbedingungen_geobasis_nrw.pdf"</a>`
@@ -79,8 +79,8 @@ export class MapConfigProviderImpl implements MapConfigProvider {
                     id: "topplus_open_light",
                     title: "TopPlus Open (Light)",
                     isBaseLayer: true,
-                    minResolution: 300,
-                    maxResolution: 500,
+                    minZoom: 10,
+                    maxZoom: 16,
                     visible: false,
                     // valid URL
                     healthCheck:
@@ -100,8 +100,8 @@ export class MapConfigProviderImpl implements MapConfigProvider {
                     type: GroupLayer,
                     id: "group_edu",
                     title: "Bildung",
-                    minResolution: 300,
-                    maxResolution: 500,
+                    minZoom: 8,
+                    maxZoom: 14,
                     layers: [
                         layerFactory.create({
                             type: SimpleLayer,
@@ -119,14 +119,14 @@ export class MapConfigProviderImpl implements MapConfigProvider {
                     type: GroupLayer,
                     title: "Verkehr",
                     id: "group_transport",
+                    minZoom: 8,
+                    maxZoom: 16,
                     layers: [
                         layerFactory.create({
                             type: SimpleLayer,
                             title: "Haltestellen Stadt Rostock",
                             id: "busstops",
                             visible: true,
-                            minResolution: 300,
-                            maxResolution: 500,
                             description:
                                 "Haltestellen des öffentlichen Personenverkehrs in der Hanse- und Universitätsstadt Rostock.",
                             olLayer: createHaltestellenLayer(),
@@ -260,8 +260,8 @@ function createStrassenLayer(layerFactory: LayerFactory) {
         id: "streets",
         title: "Straßennetz Landesbetrieb Straßenbau NRW",
         url: "https://www.wms.nrw.de/wms/strassen_nrw_wms",
-        minResolution: 300,
-        maxResolution: 500,
+        minZoom: 10,
+        maxZoom: 13,
         attributes: {
             toc: {
                 listMode: "show"
