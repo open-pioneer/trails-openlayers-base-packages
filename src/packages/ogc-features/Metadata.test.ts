809e7af6c6fd9da194931de62e82a7b2e92473ba
Jan Drewnak <j.drewnak@conterra.de>
2026-02-24 14:07:44 +0100

    Add test cases

--- src/packages/ogc-features/Metadata.test.ts
+++ src/packages/ogc-features/Metadata.test.ts
@@ -2,14 +2,57 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { describe, expect, it } from "vitest";
-import { findMatchingCrs } from "./Metadata";
+import { findMatchingCrs, getRequestCrs } from "./Metadata";
 
 describe("findMatchingCrs", () => {
-    it("matches an EPSG code against its equivalent OGC CRS URI", () => {
+    it("expect EPSG code matches its equivalent OGC CRS URI", () => {
         const available = ["http://www.opengis.net/def/crs/EPSG/0/4326"];
-        findMatchingCrs("EPSG:4326", available);
         expect(findMatchingCrs("EPSG:4326", available)).toBe(
             "http://www.opengis.net/def/crs/EPSG/0/4326"
         );
+    });
+    
+    it("expect EPSG code does not match non-equivalent OGC CRS URI", () => {
+        const available = ["http://www.opengis.net/def/crs/EPSG/0/1111"];
+        expect(findMatchingCrs("EPSG:4326", available)).toBe(undefined);
+    });
+    
+    it("expect OGC CRS URI matches same OGC CRS URI", () => {
+        const available = ["http://www.opengis.net/def/crs/EPSG/0/4326"];
+        expect(findMatchingCrs("http://www.opengis.net/def/crs/EPSG/0/4326", available)).toBe("http://www.opengis.net/def/crs/EPSG/0/4326");
+    });
+
+    it("expect returns undefined on no available CRSes", () => {
+        const emptyAvailableCRS: string[] = [];
+        expect(findMatchingCrs("http://www.opengis.net/def/crs/EPSG/0/4326", emptyAvailableCRS)).toBe(undefined);
+    });
+
+    it("expect returns undefined on no available CRSes", () => {
+        const noAvailableCRS = undefined;
+        expect(findMatchingCrs("http://www.opengis.net/def/crs/EPSG/0/4326", noAvailableCRS)).toBe(undefined);
+    });
+});
+
+
+describe("getRequestCrs", () => {
+    it("expect returns explicitly configured CRS", () => {
+        const mapCrs = "EPSG:3857";
+        const supportedCrses = ["http://www.opengis.net/def/crs/EPSG/0/4326", "http://www.opengis.net/def/crs/EPSG/0/3857"];
+        const configuredCrs = "http://www.opengis.net/def/crs/EPSG/0/1111";
+        expect(getRequestCrs(mapCrs, supportedCrses, configuredCrs)).toBe("http://www.opengis.net/def/crs/EPSG/0/1111");
+    });
+    
+    it("expect returns map CRS if supported", () => {
+        const mapCrs = "EPSG:3857";
+        const supportedCrses = ["http://www.opengis.net/def/crs/EPSG/0/4326", "http://www.opengis.net/def/crs/EPSG/0/3857"];
+        const configuredCrs = undefined;
+        expect(getRequestCrs(mapCrs, supportedCrses, configuredCrs)).toBe("http://www.opengis.net/def/crs/EPSG/0/3857");
+    });
+
+    it("expect returns CRS84 if map CRS is not supported", () => {
+        const mapCrs = "EPSG:3857";
+        const supportedCrses = ["http://www.opengis.net/def/crs/EPSG/0/4326", "http://www.opengis.net/def/crs/EPSG/0/1111"];
+        const configuredCrs = undefined;
+        expect(getRequestCrs(mapCrs, supportedCrses, configuredCrs)).toBe("http://www.opengis.net/def/crs/OGC/1.3/CRS84");
     });
 });
