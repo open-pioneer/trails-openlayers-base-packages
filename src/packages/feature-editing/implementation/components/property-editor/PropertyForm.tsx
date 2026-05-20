b275cb1d5722a7ae438b1d4e01bd22a8cf5e06e7
Michael Beckemeyer <m.beckemeyer@conterra.de>
2026-05-20 15:02:56 +0200

    Fix scrolling

--- src/packages/feature-editing/implementation/components/property-editor/PropertyForm.tsx
+++ src/packages/feature-editing/implementation/components/property-editor/PropertyForm.tsx
@@ -1,10 +1,13 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-import { Box, Flex, VStack, Text } from "@chakra-ui/react";
+import { Flex, VStack, Text } from "@chakra-ui/react";
 import { SectionHeading, TitledSection } from "@open-pioneer/react-utils";
 import { useIntl } from "open-pioneer:react-hooks";
 import { ReactNode, type ReactElement } from "react";
-import { useDeclarativeFormContext, usePropertyFormContext } from "../../context/usePropertyFormContext";
+import {
+    useDeclarativeFormContext,
+    usePropertyFormContext
+} from "../../context/usePropertyFormContext";
 import { useReactiveSnapshot } from "@open-pioneer/reactivity";
 
 export interface PropertyFormProps {
@@ -20,31 +23,42 @@ export function PropertyForm(props: PropertyFormProps): ReactElement {
 
     return (
         <TitledSection>
-            <Flex className="editor__property-form" direction={"column"} flex={1}>
+            <Flex
+                className="editor__property-form"
+                direction={"column"}
+                flex={1}
+                overflowY="hidden"
+            >
                 <SectionHeading mb={2} size="sm">
                     {heading}
                 </SectionHeading>
-                    <VStack
-                        gap={4}
-                        align="stretch"
-                        // for focus ring
-                        px="1px"
-                        pb="4px"
-                        flex={1}
-                        overflowY={"auto"}
-                    >
-                        {children}
-                    </VStack>
+                <VStack
+                    gap={4}
+                    align="stretch"
+                    // for focus ring
+                    px="1px"
+                    pb="4px"
+                    flex={1}
+                    overflowY={"auto"}
+                >
+                    {children}
+                </VStack>
                 {hasRequiredFields && (
-                <Text fontSize={"sm"} aria-hidden="true" textAlign={"right"} paddingRight={2} mb={100}>
-                    <Text as="span" color="fg.error">
-                        *
-                    </Text>{" "}
-                    {formatRichMessage({ id: "propertyEditor.requiredFieldHint" })}
-                </Text>
-                        )}
-             </Flex>
-        </TitledSection>     
+                    <Text
+                        fontSize={"sm"}
+                        aria-hidden="true"
+                        textAlign={"right"}
+                        paddingRight={2}
+                        mb={2}
+                    >
+                        <Text as="span" color="fg.error">
+                            *
+                        </Text>{" "}
+                        {formatRichMessage({ id: "propertyEditor.requiredFieldHint" })}
+                    </Text>
+                )}
+            </Flex>
+        </TitledSection>
     );
 }
 
