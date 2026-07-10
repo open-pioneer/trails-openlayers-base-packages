6da06cf309d06b65bc761b37047aa5c45c0299bf
Michael Beckemeyer <m.beckemeyer@conterra.de>
2026-07-10 12:29:55 +0200

    Replace triple slash directives with references in tsconfig.json (#643)

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 
-/// <reference types="vite/client" />
 /**
  * Global log level configured in vite.config.ts
  */
