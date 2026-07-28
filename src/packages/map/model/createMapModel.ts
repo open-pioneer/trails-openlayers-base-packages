9cc281edce419239a32b8b48f42dccd91ffd5f84
Arne Vogt <a.vogt@52north.org>
2026-07-28 16:27:17 +0200

    move topmost logic to createMapModel

--- src/packages/map/model/createMapModel.ts
+++ src/packages/map/model/createMapModel.ts
@@ -19,203 +19,209 @@ import { patchOpenLayersClassesForTesting } from "../utils/ol-test-support";
 import { registerProjections } from "../utils/projections";
 import { MapConfig } from "./MapConfig";
 import { MapModel } from "./MapModel";
+import { IS_TOPMOST_LAYER } from "../layers/shared/internals";
 
 /**
  * Register custom projection to the global proj4js definitions. User can select `EPSG:25832`
  * and `EPSG:25833` from the predefined projections without calling `registerProjections`.
  */
 registerProjections({
-    "EPSG:25832":
-        "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
-    "EPSG:25833":
-        "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
+  "EPSG:25832":
+    "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
+  "EPSG:25833":
+    "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
 });
 const LOG = createLogger(sourceId);
 
 export async function createMapModel(
-    mapId: string,
-    mapConfig: MapConfig,
-    currentIntl: ReadonlyReactive<PackageIntl>,
-    httpService: HttpService
+  mapId: string,
+  mapConfig: MapConfig,
+  currentIntl: ReadonlyReactive<PackageIntl>,
+  httpService: HttpService,
 ): Promise<MapModel> {
-    return await new MapModelFactory(mapId, mapConfig, currentIntl, httpService).createMapModel();
+  return await new MapModelFactory(mapId, mapConfig, currentIntl, httpService).createMapModel();
 }
 
 class MapModelFactory {
-    #mapId: string;
-    #mapConfig: MapConfig;
-    #currentIntl: ReadonlyReactive<PackageIntl>;
-    #httpService: HttpService;
+  #mapId: string;
+  #mapConfig: MapConfig;
+  #currentIntl: ReadonlyReactive<PackageIntl>;
+  #httpService: HttpService;
 
-    constructor(
-        mapId: string,
-        mapConfig: MapConfig,
-        currentIntl: ReadonlyReactive<PackageIntl>,
-        httpService: HttpService
-    ) {
-        this.#mapId = mapId;
-        this.#mapConfig = mapConfig;
-        this.#currentIntl = currentIntl;
-        this.#httpService = httpService;
+  constructor(
+    mapId: string,
+    mapConfig: MapConfig,
+    currentIntl: ReadonlyReactive<PackageIntl>,
+    httpService: HttpService,
+  ) {
+    this.#mapId = mapId;
+    this.#mapConfig = mapConfig;
+    this.#currentIntl = currentIntl;
+    this.#httpService = httpService;
+  }
+
+  async createMapModel() {
+    const mapId = this.#mapId;
+    const mapConfig = this.#mapConfig;
+    const { view: viewOption, ...rawOlOptions } = mapConfig.advanced ?? {};
+    const showDefaultAttributions =
+      mapConfig.showAttributions ?? (rawOlOptions.controls ? false : true);
+
+    const mapOptions: MapOptions = {
+      ...rawOlOptions,
+    };
+    if (!mapOptions.controls) {
+      mapOptions.controls = [];
+    }
+    if (!mapOptions.interactions) {
+      const shiftCtrlKeysOnly = (
+        mapBrowserEvent: MapBrowserEvent<KeyboardEvent | WheelEvent | PointerEvent>,
+      ) => {
+        const originalEvent = mapBrowserEvent.originalEvent;
+        return (originalEvent.metaKey || originalEvent.ctrlKey) && originalEvent.shiftKey;
+      };
+
+      // setting altShiftDragRotate to false disables or excludes DragRotate interaction
+      mapOptions.interactions = defaultInteractions({
+        dragPan: true,
+        altShiftDragRotate: false,
+        pinchRotate: false,
+        mouseWheelZoom: true,
+      }).extend([new DragZoom({ out: true, condition: shiftCtrlKeysOnly })]);
     }
 
-    async createMapModel() {
-        const mapId = this.#mapId;
-        const mapConfig = this.#mapConfig;
-        const { view: viewOption, ...rawOlOptions } = mapConfig.advanced ?? {};
-        const showDefaultAttributions =
-            mapConfig.showAttributions ?? (rawOlOptions.controls ? false : true);
+    const view = (await viewOption) ?? {};
+    this.#initializeViewOptions(view);
+    mapOptions.view = view instanceof View ? view : new View(view);
 
-        const mapOptions: MapOptions = {
-            ...rawOlOptions
-        };
-        if (!mapOptions.controls) {
-            mapOptions.controls = [];
+    if (!mapOptions.layers && !mapConfig.layers) {
+      mapOptions.layers = [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ];
+    }
+
+    const initialView = mapConfig.initialView;
+    const initialExtent = initialView?.kind === "extent" ? initialView.extent : undefined;
+
+    LOG.debug(`Constructing OpenLayers map with options`, mapOptions);
+
+    if (import.meta.env.VITEST) {
+      patchOpenLayersClassesForTesting();
+    }
+
+    const olMap = new OlMap(mapOptions);
+    const mapModel = new MapModel(
+      {
+        id: mapId,
+        olMap,
+        initialExtent,
+        showDefaultAttributions,
+        currentIntl: this.#currentIntl,
+        httpService: this.#httpService,
+      },
+      INTERNAL_CONSTRUCTOR_TAG,
+    );
+
+    return batch(() => {
+      try {
+        if (mapConfig.layers) {
+          for (const layerConfig of mapConfig.layers) {
+            const isTopMostLayer = layerConfig[IS_TOPMOST_LAYER]();
+            if (isTopMostLayer) {
+              mapModel.layers.addLayer(layerConfig, { at: "topmost" });
+            } else {
+              mapModel.layers.addLayer(layerConfig);
+            }
+          }
         }
-        if (!mapOptions.interactions) {
-            const shiftCtrlKeysOnly = (
-                mapBrowserEvent: MapBrowserEvent<KeyboardEvent | WheelEvent | PointerEvent>
-            ) => {
-                const originalEvent = mapBrowserEvent.originalEvent;
-                return (originalEvent.metaKey || originalEvent.ctrlKey) && originalEvent.shiftKey;
-            };
+        return mapModel;
+      } catch (e) {
+        mapModel.destroy();
+        throw e;
+      }
+    });
+  }
 
-            // setting altShiftDragRotate to false disables or excludes DragRotate interaction
-            mapOptions.interactions = defaultInteractions({
-                dragPan: true,
-                altShiftDragRotate: false,
-                pinchRotate: false,
-                mouseWheelZoom: true
-            }).extend([new DragZoom({ out: true, condition: shiftCtrlKeysOnly })]);
-        }
-
-        const view = (await viewOption) ?? {};
-        this.#initializeViewOptions(view);
-        mapOptions.view = view instanceof View ? view : new View(view);
-
-        if (!mapOptions.layers && !mapConfig.layers) {
-            mapOptions.layers = [
-                new TileLayer({
-                    source: new OSM()
-                })
-            ];
-        }
-
-        const initialView = mapConfig.initialView;
-        const initialExtent = initialView?.kind === "extent" ? initialView.extent : undefined;
-
-        LOG.debug(`Constructing OpenLayers map with options`, mapOptions);
-
-        if (import.meta.env.VITEST) {
-            patchOpenLayersClassesForTesting();
-        }
-
-        const olMap = new OlMap(mapOptions);
-        const mapModel = new MapModel(
-            {
-                id: mapId,
-                olMap,
-                initialExtent,
-                showDefaultAttributions,
-                currentIntl: this.#currentIntl,
-                httpService: this.#httpService
-            },
-            INTERNAL_CONSTRUCTOR_TAG
+  #initializeViewOptions(view: View | ViewOptions) {
+    const mapId = this.#mapId;
+    const mapConfig = this.#mapConfig;
+    if (view instanceof View) {
+      const warn = (prop: string) => {
+        LOG.warn(
+          `The advanced configuration for map id '${mapId}' has provided a fully constructed view instance: ${prop} cannot be applied.\n` +
+            `Use ViewOptions instead of a View instance.`,
         );
+      };
 
-        return batch(() => {
-            try {
-                if (mapConfig.layers) {
-                    for (const layerConfig of mapConfig.layers) {
-                        mapModel.layers.addLayer(layerConfig);
-                    }
-                }
-                return mapModel;
-            } catch (e) {
-                mapModel.destroy();
-                throw e;
-            }
-        });
+      if (mapConfig.projection != null) {
+        warn("projection");
+      }
+      if (mapConfig.initialView != null) {
+        warn("initialView");
+      }
+      return;
     }
 
-    #initializeViewOptions(view: View | ViewOptions) {
-        const mapId = this.#mapId;
-        const mapConfig = this.#mapConfig;
-        if (view instanceof View) {
-            const warn = (prop: string) => {
-                LOG.warn(
-                    `The advanced configuration for map id '${mapId}' has provided a fully constructed view instance: ${prop} cannot be applied.\n` +
-                        `Use ViewOptions instead of a View instance.`
-                );
-            };
-
-            if (mapConfig.projection != null) {
-                warn("projection");
-            }
-            if (mapConfig.initialView != null) {
-                warn("initialView");
-            }
-            return;
-        }
-
-        const projection = (view.projection = this.#initializeProjection(mapConfig.projection));
-        const initialView = mapConfig.initialView;
-        if (initialView) {
-            switch (initialView.kind) {
-                case "position":
-                    view.zoom = initialView.zoom;
-                    view.center = [initialView.center.x, initialView.center.y];
-                    break;
-                case "extent": {
-                    /*
+    const projection = (view.projection = this.#initializeProjection(mapConfig.projection));
+    const initialView = mapConfig.initialView;
+    if (initialView) {
+      switch (initialView.kind) {
+        case "position":
+          view.zoom = initialView.zoom;
+          view.center = [initialView.center.x, initialView.center.y];
+          break;
+        case "extent": {
+          /*
                         OpenLayers does not support configuration of the initial map extent.
                         The only relevant options here are center, zoom (and resolution).
                         We must set those values because otherwise OpenLayers will not initialize layer sources.
 
                         The actual initial extent is applied once tha map has loaded and its size is known.
                     */
-                    const extent = initialView.extent;
-                    view.zoom = 0;
-                    view.center = [
-                        extent.xMin + (extent.xMax - extent.xMin) / 2,
-                        extent.yMin + (extent.yMax - extent.yMin) / 2
-                    ];
-                    break;
-                }
-            }
-        } else {
-            this.#setViewDefaults(view, projection);
+          const extent = initialView.extent;
+          view.zoom = 0;
+          view.center = [
+            extent.xMin + (extent.xMax - extent.xMin) / 2,
+            extent.yMin + (extent.yMax - extent.yMin) / 2,
+          ];
+          break;
         }
+      }
+    } else {
+      this.#setViewDefaults(view, projection);
+    }
+  }
+
+  #setViewDefaults(view: ViewOptions, projection: Projection) {
+    if (view.center == null) {
+      const extent = projection.getExtent(); // can be null
+      if (!extent) {
+        LOG.warn(
+          `Cannot set default center coordinate because the current projection has no associated extent.\n` +
+            `Try to configure 'initialView' explicity.`,
+        );
+      } else {
+        view.center = getCenter(extent);
+      }
     }
 
-    #setViewDefaults(view: ViewOptions, projection: Projection) {
-        if (view.center == null) {
-            const extent = projection.getExtent(); // can be null
-            if (!extent) {
-                LOG.warn(
-                    `Cannot set default center coordinate because the current projection has no associated extent.\n` +
-                        `Try to configure 'initialView' explicity.`
-                );
-            } else {
-                view.center = getCenter(extent);
-            }
-        }
+    if (view.zoom == null || view.resolution == null) {
+      view.zoom = 0;
+    }
+  }
 
-        if (view.zoom == null || view.resolution == null) {
-            view.zoom = 0;
-        }
+  #initializeProjection(projectionOption: MapConfig["projection"]) {
+    if (projectionOption == null) {
+      // oxlint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return getProjection("EPSG:3857")!; // default OpenLayers projection
     }
 
-    #initializeProjection(projectionOption: MapConfig["projection"]) {
-        if (projectionOption == null) {
-            // oxlint-disable-next-line @typescript-eslint/no-non-null-assertion
-            return getProjection("EPSG:3857")!; // default OpenLayers projection
-        }
-
-        const projection = getProjection(projectionOption);
-        if (!projection) {
-            throw new Error(`Failed to retrieve projection for code '${projectionOption}'.`);
-        }
-        return projection;
+    const projection = getProjection(projectionOption);
+    if (!projection) {
+      throw new Error(`Failed to retrieve projection for code '${projectionOption}'.`);
     }
+    return projection;
+  }
 }
